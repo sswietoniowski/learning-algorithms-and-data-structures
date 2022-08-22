8d14b53ccd29bfed2198fe8d8481694203f42e83
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-22 12:56:52 +0200

    Added initial solution

--- neetcode/Solution.cs
+++ neetcode/Solution.cs
@@ -76,7 +76,30 @@
         // https://youtu.be/vzdNOK2oB2E
         public IList<IList<string>> GroupAnagrams(string[] strs)
         {
-            throw new NotImplementedException();
+            var result = new List<IList<string>>();
+            var wordsSet = new HashSet<string>();
+            var anagramsDictionary = new Dictionary<string, IList<string>>();
+
+            foreach (var word in strs)
+            {
+                char[] chars = word.ToCharArray();
+                Array.Sort(chars);
+                var key = new String(chars);
+                if (!wordsSet.Contains(key))
+                {
+                    wordsSet.Add(key);
+                    anagramsDictionary[key] = new List<string>();
+                }
+
+                anagramsDictionary[key].Add(word);
+            }
+
+            foreach (var kvp in anagramsDictionary)
+            {
+                result.Add(kvp.Value);
+            }
+
+            return result;
         }
     }
 }
