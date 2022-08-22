c530c71a89d598c31f78edb3ac3072d455eb8b7b
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-22 15:43:16 +0200

    Implemented basic solution

--- neetcode/Codec.cs
+++ neetcode/Codec.cs
@@ -1,20 +1,102 @@
-﻿namespace neetcode
+﻿using System.Text;
+
+namespace neetcode
 {
     // https://leetcode.com/problems/encode-and-decode-strings/
     // https://youtu.be/B1k_sxOSgv8
     public class Codec
     {
+        private readonly HashSet<char> escapedCharacters = new HashSet<char>()
+    {
+        '[', ']', '"', ',', '\\'
+    };
 
         // Encodes a list of strings to a single string.
         public string encode(IList<string> strs)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+            sb.Append('[');
+            bool addSeparator = false;
+            foreach (var s in strs)
+            {
+                if (addSeparator)
+                {
+                    sb.Append(',');
+                }
+
+                sb.Append('"');
+
+                foreach (var ch in s)
+                {
+                    if (escapedCharacters.Contains(ch))
+                    {
+                        sb.Append('\\');
+                    }
+
+                    sb.Append(ch);
+                }
+
+                sb.Append('"');
+
+                addSeparator = true;
+            }
+            sb.Append(']');
+
+            return sb.ToString();
         }
 
         // Decodes a single string to a list of strings.
         public IList<string> decode(string s)
         {
-            throw new NotImplementedException();
+            var result = new List<string>();
+
+            if (s.StartsWith('['))
+            {
+                s = s.Substring(1);
+            }
+
+            if (s.EndsWith(']'))
+            {
+                s = s.Substring(0, s.Length - 1);
+            }
+
+            var inWord = false;
+            var word = new StringBuilder();
+            var escape = false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!escape && s[i] == '\\')
+                {
+                    escape = true;
+                    continue;
+                }
+
+                if (!inWord && s[i] == '"')
+                {
+                    inWord = true;
+                    escape = false;
+                    continue;
+                }
+
+                if (inWord && !escape && s[i] == '"')
+                {
+                    result.Add(word.ToString());
+                    word.Clear();
+                    escape = false;
+                    inWord = false;
+                    continue;
+                }
+
+                if (!inWord && s[i] == ',')
+                {
+                    continue;
+                }
+
+                word.Append(s[i]);
+                escape = false;
+            }
+
+            return result;
         }
     }
 
