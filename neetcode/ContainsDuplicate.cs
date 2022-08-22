3cb293784128d3233b3e473a24696edb50b82be3
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-22 08:36:27 +0200

    Created starter excersises

---
+++ neetcode/ContainsDuplicate.cs
@@ -0,0 +1,20 @@
+﻿namespace neetcode
+{
+    public class Solution
+    {
+        // https://leetcode.com/problems/contains-duplicate/
+        // https://youtu.be/3OamzN90kPg
+        public bool ContainsDuplicate(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        // https://leetcode.com/problems/group-anagrams/
+        // https://youtu.be/vzdNOK2oB2E
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
