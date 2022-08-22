758c63e720aaef7c62eab7c261bdb0ac8307947f
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-22 10:55:33 +0200

    Updated starter project

---
+++ neetcode/Codec.cs
@@ -0,0 +1,24 @@
+﻿namespace neetcode
+{
+    // https://leetcode.com/problems/encode-and-decode-strings/
+    // https://youtu.be/B1k_sxOSgv8
+    public class Codec
+    {
+
+        // Encodes a list of strings to a single string.
+        public string encode(IList<string> strs)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Decodes a single string to a list of strings.
+        public IList<string> decode(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // Your Codec object will be instantiated and called as such:
+    // Codec codec = new Codec();
+    // codec.decode(codec.encode(strs));
+}
