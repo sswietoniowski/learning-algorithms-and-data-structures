02ee0c11558993196e1bc59fb3fb2b76aa8ac332
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-23 11:10:05 +0200

    Solved problem "Single Number"

--- neetcode/Solution.cs
+++ neetcode/Solution.cs
@@ -178,5 +178,39 @@
 
             return null;
         }
+
+        // https://leetcode.com/problems/single-number/
+        // https://youtu.be/qMPX1AOa83k
+        public int SingleNumber(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+
+            Dictionary<int, int> occurrences = new();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (occurrences.ContainsKey(nums[i]))
+                {
+                    occurrences[nums[i]] += 1;
+                }
+                else
+                {
+                    occurrences.Add(nums[i], 1);
+                }
+            }
+
+            foreach (var kvp in occurrences)
+            {
+                if (kvp.Value == 1)
+                {
+                    return kvp.Key;
+                }
+            }
+
+            throw new ArgumentException();
+        }
     }
 }
