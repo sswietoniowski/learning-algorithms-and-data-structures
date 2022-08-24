20609a0b543e5727b47cf0990435015ffaf92816
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-24 19:08:16 +0200

    Solved Google's interview problem

--- neetcode/Solution.cs
+++ neetcode/Solution.cs
@@ -386,5 +386,43 @@ namespace neetcode
             return new int[] { -1, -1 };
         }
 
+        // https://leetcode.com/explore/interview/card/google/67/sql-2/3044/
+        public int NumUniqueEmails(string[] emails)
+        {
+            var uniqueEmails = new HashSet<string>();
+
+            foreach (var email in emails)
+            {
+                string[] parts = email.Split('@');
+                string username = parts[0];
+                string domain = parts[1];
+                int plusPosition = username.IndexOf('+');
+                if (plusPosition > 0)
+                {
+                    username = username.Substring(0, plusPosition);
+                }
+                string rest = username;
+                int dotPosition = rest.IndexOf('.');
+                StringBuilder sb = new();
+                while (dotPosition > 0)
+                {
+                    sb.Append(rest.Substring(0, dotPosition));
+                    if (dotPosition < rest.Length - 1)
+                    {
+                        rest = rest.Substring(dotPosition + 1, rest.Length - dotPosition - 1);
+                    }
+                    dotPosition = rest.IndexOf('.');
+                }
+                if (rest.Length > 0)
+                {
+                    sb.Append(rest);
+                }
+                username = sb.ToString();
+                uniqueEmails.Add($"{username}@{domain}");
+            }
+
+            return uniqueEmails.Count;
+        }
+
     }
 }
