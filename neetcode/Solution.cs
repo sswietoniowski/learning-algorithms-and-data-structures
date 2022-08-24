25a9d0f0233a5e6aa0334427dc68833b0047136b
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-24 22:17:33 +0200

    Solved "Evaluate Reverse Polish Notation"

--- neetcode/Solution.cs
+++ neetcode/Solution.cs
@@ -424,5 +424,36 @@ namespace neetcode
             return uniqueEmails.Count;
         }
 
+        // https://leetcode.com/problems/evaluate-reverse-polish-notation/
+        public int EvalRPN(string[] tokens)
+        {
+            var operators = new HashSet<string>() { "+", "-", "*", "/" };
+            var stack = new Stack<int>();
+
+            foreach (var token in tokens)
+            {
+                if (operators.Contains(token))
+                {
+                    int b = stack.Pop();
+                    int a = stack.Pop();
+                    int c = token switch
+                    {
+                        "+" => a + b,
+                        "-" => a - b,
+                        "*" => a * b,
+                        "/" => a / b,
+                        _ => 0
+                    };
+
+                    stack.Push(c);
+                }
+                else
+                {
+                    stack.Push(Convert.ToInt32(token));
+                }
+            }
+
+            return stack.Pop();
+        }
     }
 }
