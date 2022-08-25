46dcf92ace835bad0187dfbb16b2d3f9677dd9f1
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-25 10:40:56 +0200

    Improved solution

--- neetcode/Solution.cs
+++ neetcode/Solution.cs
@@ -542,47 +542,72 @@ namespace neetcode
         // https://youtu.be/XIdigk956u0
         public ListNode MergeTwoLists(ListNode list1, ListNode list2)
         {
-            ListNode head = null;
-            ListNode currentNode = null;
-            ListNode previousNode = null;
+            // v1
+            //ListNode head = null;
+            //ListNode currentNode = null;
+            //ListNode previousNode = null;
 
-            while (list1 != null || list2 != null)
+            //while (list1 != null || list2 != null)
+            //{
+            //    if (list1 == null)
+            //    {
+            //        currentNode = new ListNode(list2.val, null);
+            //        list2 = list2.next;
+            //    }
+            //    else if (list2 == null)
+            //    {
+            //        currentNode = new ListNode(list1.val, null);
+            //        list1 = list1.next;
+            //    }
+            //    else if (list1.val <= list2.val)
+            //    {
+            //        currentNode = new ListNode(list1.val, null);
+            //        list1 = list1.next;
+            //    }
+            //    else
+            //    {
+            //        currentNode = new ListNode(list2.val, null);
+            //        list2 = list2.next;
+            //    }
+
+            //    if (head == null)
+            //    {
+            //        head = currentNode;
+            //    }
+
+            //    if (previousNode != null)
+            //    {
+            //        previousNode.next = currentNode;
+            //    }
+
+            //    previousNode = currentNode;
+            //}
+
+            //return head;
+
+            // v2
+            ListNode dummyHead = new ListNode(0);
+            ListNode previousHead = dummyHead;
+
+            while (list1 != null && list2 != null)
             {
-                if (list1 == null)
+                if (list1.val <= list2.val)
                 {
-                    currentNode = new ListNode(list2.val, null);
-                    list2 = list2.next;
-                }
-                else if (list2 == null)
-                {
-                    currentNode = new ListNode(list1.val, null);
-                    list1 = list1.next;
-                }
-                else if (list1.val <= list2.val)
-                {
-                    currentNode = new ListNode(list1.val, null);
+                    previousHead.next = list1;
                     list1 = list1.next;
                 }
                 else
                 {
-                    currentNode = new ListNode(list2.val, null);
+                    previousHead.next = list2;
                     list2 = list2.next;
                 }
 
-                if (head == null)
-                {
-                    head = currentNode;
-                }
-
-                if (previousNode != null)
-                {
-                    previousNode.next = currentNode;
-                }
-
-                previousNode = currentNode;
+                previousHead = previousHead.next;
             }
 
-            return head;
+            previousHead.next = list1 == null ? list2 : list1;
+
+            return dummyHead.next;
         }
     }
 }
