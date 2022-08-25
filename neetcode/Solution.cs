99af1bf9981eb7bf5ef2d089cb21cc0839756b51
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-25 06:29:56 +0200

    Solved "Valid Parentheses"

--- neetcode/Solution.cs
+++ neetcode/Solution.cs
@@ -455,5 +455,47 @@ namespace neetcode
 
             return stack.Pop();
         }
+
+        private bool IsValidPair(char opening, char closing)
+        {
+            return (opening == '(' && closing == ')') ||
+                   (opening == '[' && closing == ']') ||
+                   (opening == '{' && closing == '}');
+        }
+
+        // https://leetcode.com/problems/valid-parentheses/
+        // https://youtu.be/WTzjTskDFMg
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var ch in s)
+            {
+                if ("([{".Contains(ch))
+                {
+                    stack.Push(ch);
+                    continue;
+                }
+
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+
+                var opening = stack.Pop();
+
+                if (!IsValidPair(opening, ch))
+                {
+                    return false;
+                }
+            }
+
+            if (stack.Count > 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
