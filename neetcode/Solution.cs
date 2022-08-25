3dff53d500210f47a84e702f68534efb894b1d6f
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-25 10:22:09 +0200

    Solved "Merge Two Sorted Lists"

--- neetcode/Solution.cs
+++ neetcode/Solution.cs
@@ -537,5 +537,52 @@ namespace neetcode
 
             return newHead;
         }
+
+        // https://leetcode.com/problems/merge-two-sorted-lists/
+        // https://youtu.be/XIdigk956u0
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode head = null;
+            ListNode currentNode = null;
+            ListNode previousNode = null;
+
+            while (list1 != null || list2 != null)
+            {
+                if (list1 == null)
+                {
+                    currentNode = new ListNode(list2.val, null);
+                    list2 = list2.next;
+                }
+                else if (list2 == null)
+                {
+                    currentNode = new ListNode(list1.val, null);
+                    list1 = list1.next;
+                }
+                else if (list1.val <= list2.val)
+                {
+                    currentNode = new ListNode(list1.val, null);
+                    list1 = list1.next;
+                }
+                else
+                {
+                    currentNode = new ListNode(list2.val, null);
+                    list2 = list2.next;
+                }
+
+                if (head == null)
+                {
+                    head = currentNode;
+                }
+
+                if (previousNode != null)
+                {
+                    previousNode.next = currentNode;
+                }
+
+                previousNode = currentNode;
+            }
+
+            return head;
+        }
     }
 }
