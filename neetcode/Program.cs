ac4fddecfd95954322a632d36b4f13305c1d7e41
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-26 08:32:47 +0200

    Solved "Happy Number"

--- neetcode/Program.cs
+++ neetcode/Program.cs
@@ -1,9 +1,3 @@
 ﻿using neetcode;
 
 Console.WriteLine("Hello Algorithms & Data Structures");
-
-string[] emails = {"test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"};
-
-Solution solution = new Solution();
-
-Console.WriteLine(solution.NumUniqueEmails(emails));
--- neetcode/Solution.cs
+++ neetcode/Solution.cs
@@ -638,5 +638,37 @@ namespace neetcode
 
             return -1;
         }
+
+        private int SumDigitsSquares(int n)
+        {
+            int result = 0;
+            while (n > 0)
+            {
+                int digit = n % 10;
+                result += digit * digit;
+                n /= 10;
+            }
+            return result;
+        }
+
+
+        // https://leetcode.com/problems/happy-number/
+        // https://youtu.be/ljz85bxOYJ0
+        public bool IsHappy(int n)
+        {
+            var testedNumbers = new HashSet<int>();
+
+            while (n != 1)
+            {
+                n = SumDigitsSquares(n);
+                if (testedNumbers.Contains(n))
+                {
+                    return false;
+                }
+                testedNumbers.Add(n);
+            }
+
+            return true;
+        }
     }
 }
