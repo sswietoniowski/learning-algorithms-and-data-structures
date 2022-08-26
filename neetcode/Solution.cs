dc7b275726b7bcfa7cd9d4b06cd2a670f3f7276d
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-26 08:08:38 +0200

    Solved "Binary Search"

--- neetcode/Solution.cs
+++ neetcode/Solution.cs
@@ -609,5 +609,34 @@ namespace neetcode
 
             return dummyHead.next;
         }
+
+        // https://leetcode.com/problems/binary-search/
+        // https://youtu.be/s4DPM8ct1pI
+        public int BinarySearch(int[] nums, int target)
+        {
+            int low = 0;
+            int mid;
+            int high = nums.Length - 1;
+
+            while (low <= high)
+            {
+                mid = low + (high - low) / 2;
+
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (nums[mid] < target)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+
+            return -1;
+        }
     }
 }
