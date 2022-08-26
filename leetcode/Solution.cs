08e165c19d5013c74ee07e035c187c024d4e0f1c
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-26 16:17:44 +0200

    Solved "Longest Common Prefix"

--- leetcode/Solution.cs
+++ leetcode/Solution.cs
@@ -1,6 +1,54 @@
-﻿namespace leetcode
+﻿using System.Text;
+
+namespace leetcode
 {
     public class Solution
     {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            int n = strs.Length;
+
+            if (n == 1)
+            {
+                return strs[0];
+            }
+
+            int maxLength = 200;
+            foreach (var str in strs)
+            {
+                if (maxLength > str.Length)
+                {
+                    maxLength = str.Length;
+                }
+            }
+
+            if (maxLength == 0)
+            {
+                return "";
+            }
+
+            StringBuilder sb = new();
+            for (int i = 0; i < maxLength; i++)
+            {
+                char current = strs[0][i];
+                bool isCommon = true;
+
+                for (int j = 1; j < n; j++)
+                {
+                    if (strs[j][i] != current)
+                    {
+                        isCommon = false;
+                        break;
+                    }
+                }
+
+                if (!isCommon)
+                {
+                    break;
+                }
+
+                sb.Append(strs[0][i]);
+            }
+            return sb.ToString();
+        }
     }
-}
