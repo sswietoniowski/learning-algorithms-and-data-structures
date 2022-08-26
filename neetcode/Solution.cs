d63ebe63a30776e991de163fabc60a097196f121
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-26 12:13:21 +0200

    Solved "Plus One"

--- neetcode/Solution.cs
+++ neetcode/Solution.cs
@@ -670,5 +670,38 @@ namespace neetcode
 
             return true;
         }
+
+        // https://leetcode.com/problems/plus-one/
+        // https://youtu.be/jIaA8boiG1s
+        public int[] PlusOne(int[] digits)
+        {
+            int[] result = new int[digits.Length];
+            int i = digits.Length - 1;
+            bool add = true;
+            int digit = 0;
+            while (i >= 0)
+            {
+                digit = digits[i] + (add ? 1 : 0);
+                if (digit >= 10)
+                {
+                    result[i] = digit % 10;
+                    add = true;
+                }
+                else
+                {
+                    result[i] = digit;
+                    add = false;
+                }
+                i--;
+            }
+            if (add)
+            {
+                int[] newResult = new int[result.Length + 1];
+                newResult[0] = 1;
+                Array.Copy(result, 0, newResult, 1, result.Length);
+                result = newResult;
+            }
+            return result;
+        }
     }
 }
