a6745c175517ffa171ddb68f09d4c58f99b4f69a
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-27 13:21:38 +0200

    Solved "Best Time to Buy and Sell Stock"

--- neetcode/Solution.cs
+++ neetcode/Solution.cs
@@ -703,5 +703,34 @@ namespace neetcode
             }
             return result;
         }
+
+        // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+        // https://youtu.be/1pkOgXD63yU
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+            int buyDay = 0;
+            int sellDay = 1;
+
+            while (buyDay < prices.Length && sellDay < prices.Length)
+            {
+                if (prices[buyDay] >= prices[sellDay])
+                {
+                    buyDay = sellDay;
+                    sellDay = buyDay + 1;
+                }
+                else
+                {
+                    if (prices[sellDay] - prices[buyDay] > maxProfit)
+                    {
+                        maxProfit = prices[sellDay] - prices[buyDay];
+                    }
+
+                    sellDay = sellDay + 1;
+                }
+            }
+
+            return maxProfit;
+        }
     }
 }
