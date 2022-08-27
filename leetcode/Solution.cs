8e1879b2586e4e3b8ec4ddbb0b3d9b119101b996
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-27 08:08:33 +0200

    Solved "Remove Duplicates from Sorted Array"

--- leetcode/Solution.cs
+++ leetcode/Solution.cs
@@ -52,5 +52,20 @@ namespace leetcode
 
             return sb.ToString();
         }
+
+        // https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+        public int RemoveDuplicates(int[] nums)
+        {
+            int k = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[k] != nums[i])
+                {
+                    k++;
+                    nums[k] = nums[i];
+                }
+            }
+            return k + 1;
+        }
     }
 }
