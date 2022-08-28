d194f4b5f1dcca5d9789b586a2f5a06a856a7653
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-28 08:26:40 +0200

    Improved solution

--- leetcode/P0009_PalindromeNumber/Solution.cs
+++ leetcode/P0009_PalindromeNumber/Solution.cs
@@ -2,22 +2,41 @@
 {
     public class Solution
     {
-        public bool IsPalindrome(string s)
-        {
-            for (int i = 0; i < s.Length / 2; i++)
-            {
-                if (s[i] != s[s.Length - 1 - i])
-                {
-                    return false;
-                }
-            }
+        // v1
+        //public bool IsPalindrome(string s)
+        //{
+        //    for (int i = 0; i < s.Length / 2; i++)
+        //    {
+        //        if (s[i] != s[s.Length - 1 - i])
+        //        {
+        //            return false;
+        //        }
+        //    }
 
-            return true;
-        }
+        //    return true;
+        //}
 
+        //public bool IsPalindrome(int x)
+        //{
+        //    return IsPalindrome(x.ToString());
+        //}
+
+        // v2
         public bool IsPalindrome(int x)
         {
-            return IsPalindrome(x.ToString());
+            if (x < 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+
+            int revertedNumber = 0;
+            while (x > revertedNumber)
+            {
+                revertedNumber = revertedNumber * 10 + x % 10;
+                x /= 10;
+            }
+
+            return x == revertedNumber || x == revertedNumber / 10;
         }
     }
 }
