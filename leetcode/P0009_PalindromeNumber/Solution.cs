dfbf77e4a6ab0a2854f8fb10f60cca46f45d4f4a
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-28 08:20:11 +0200

    Solved "Palindrome Number"

---
+++ leetcode/P0009_PalindromeNumber/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode.P0009_PalindromeNumber
+{
+    public class Solution
+    {
+        public bool IsPalindrome(string s)
+        {
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool IsPalindrome(int x)
+        {
+            return IsPalindrome(x.ToString());
+        }
+    }
+}
