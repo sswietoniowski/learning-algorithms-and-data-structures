e841be531e697f6400bf609ea6fe773a4d4b4bf8
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-29 21:16:04 +0200

    Solved "Climbing Stairs"

--- neetcode/P0070_ClimbingStairs/Solution.cs
+++ neetcode/P0070_ClimbingStairs/Solution.cs
@@ -2,5 +2,15 @@
 {
     public class Solution
     {
+        public int ClimbStairs(int n)
+        {
+            int one = 1;
+            int two = 1;
+            for (int i = 0; i < n - 1; i++)
+            {
+                (one, two) = (one + two, one);
+            }
+            return one;
+        }
     }
 }
