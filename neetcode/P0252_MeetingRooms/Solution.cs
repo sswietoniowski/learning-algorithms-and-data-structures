07356f6306bb36eb2bb1c2da940e952c1a760fe9
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-29 20:35:16 +0200

    Solved "Meeting Rooms"

--- neetcode/P0252_MeetingRooms/Solution.cs
+++ neetcode/P0252_MeetingRooms/Solution.cs
@@ -1,10 +1,47 @@
-﻿namespace neetcode.P0252_MeetingRooms
+﻿using System.Collections;
+
+namespace neetcode.P0252_MeetingRooms
 {
     public class Solution
     {
+        public class StartComparer : IComparer
+        {
+            public int Compare(Object x, Object y)
+            {
+                int[] first = x as int[];
+                int[] second = y as int[];
+
+                if (first == null || second == null)
+                {
+                    throw new ArgumentException();
+                }
+
+                if (first[0] > second[0])
+                {
+                    return 1;
+                }
+                else if (first[0] < second[0])
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
         public bool CanAttendMeetings(int[][] intervals)
         {
-            throw new NotImplementedException();
+            Array.Sort(intervals, new StartComparer());
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                if (intervals[i - 1][1] > intervals[i][0])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
