29436e25a2adcb1be32bbb6e2f293156720b323d
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-29 19:40:42 +0200

    Prepared starter code

---
+++ neetcode/P0252_MeetingRooms/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace neetcode.P0252_MeetingRooms
+{
+    public class Solution
+    {
+        public bool CanAttendMeetings(int[][] intervals)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
