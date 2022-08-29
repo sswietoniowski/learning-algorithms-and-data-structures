6dae8135e573dc800cee686319014c65bd4fcfde
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-29 15:19:16 +0200

    Improved solution

--- leetcode/P0094_BinaryTreeInorderTraversal/Solution.cs
+++ leetcode/P0094_BinaryTreeInorderTraversal/Solution.cs
@@ -15,27 +15,46 @@
 
     public class Solution
     {
+        // v1
+        //public IList<int> InorderTraversal(TreeNode root)
+        //{
+        //    var result = new LinkedList<int>();
+        //    if (root == null)
+        //    {
+        //        return result.ToList();
+        //    }
+
+        //    var left = InorderTraversal(root.left);
+        //    foreach (var item in left)
+        //    {
+        //        result.AddLast(item);
+        //    }
+        //    result.AddLast(root.val);
+        //    var right = InorderTraversal(root.right);
+        //    foreach (var item in right)
+        //    {
+        //        result.AddLast(item);
+        //    }
+
+        //    return result.ToList();
+        //}
+
+        // v2
+        private void add(TreeNode root, List<int> result)
+        {
+            if (root != null)
+            {
+                add(root.left, result);
+                result.Add(root.val);
+                add(root.right, result);
+            }
+        }
+
         public IList<int> InorderTraversal(TreeNode root)
         {
-            var result = new LinkedList<int>();
-            if (root == null)
-            {
-                return result.ToList();
-            }
-
-            var left = InorderTraversal(root.left);
-            foreach (var item in left)
-            {
-                result.AddLast(item);
-            }
-            result.AddLast(root.val);
-            var right = InorderTraversal(root.right);
-            foreach (var item in right)
-            {
-                result.AddLast(item);
-            }
-
-            return result.ToList();
+            var result = new List<int>();
+            add(root, result);
+            return result;
         }
     }
 }
