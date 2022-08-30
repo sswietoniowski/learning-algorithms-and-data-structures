92550626d3228474d039eacd227959d63e8db369
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-30 08:10:31 +0200

    Solved "Remove Duplicates from Sorted List"

---
+++ leetcode/P0083_RemoveDuplicatesFromSortedList/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace leetcode.P0083_RemoveDuplicatesFromSortedList
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null)
+            {
+                return head;
+            }
+
+            ListNode previous = head;
+            ListNode current = head.next;
+
+            while (current != null)
+            {
+                if (current.val == previous.val)
+                {
+                    previous.next = current.next;
+                }
+                else
+                {
+                    previous = current;
+                }
+
+                current = current.next;
+            }
+
+            return head;
+        }
+    }
+}
