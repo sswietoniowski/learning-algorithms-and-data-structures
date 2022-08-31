3f8076e0aab0ce0afcdb33e624513f2b9ebb7298
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-31 10:40:58 +0200

    Solved "Last Stone Weight"

---
+++ neetcode/P1046_LastStoneWeight/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace neetcode.P1046_LastStoneWeight
+{
+    public class Solution
+    {
+        public int LastStoneWeight(int[] stones)
+        {
+            var pq = new PriorityQueue<int, int>();
+            foreach (var stone in stones)
+            {
+                pq.Enqueue(stone, -stone);
+            }
+            while (pq.Count > 1)
+            {
+                var a = pq.Dequeue();
+                var b = pq.Dequeue();
+                if (a != b)
+                {
+                    pq.Enqueue(a - b, -(a - b));
+                }
+            }
+            return pq.Count == 0 ? 0 : pq.Dequeue();
+        }
+    }
+}
