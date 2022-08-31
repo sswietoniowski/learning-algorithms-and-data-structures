f22cb21520fa35fafb4fa6b755398183b57c9a88
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-31 13:14:06 +0200

    Solved "Implement Trie"

---
+++ neetcode/P0208_ImplementTrie/Solution.cs
@@ -0,0 +1,57 @@
+﻿namespace neetcode.P0208_ImplementTrie
+{
+    public class Node
+    {
+        public Dictionary<char, Node> children = new();
+        public bool isCompleteWord = false;
+    }
+
+    // https://leetcode.com/problems/implement-trie-prefix-tree/
+    // https://youtu.be/oobqoCJlHA0
+    public class Trie
+    {
+        private Node root = new();
+        
+        public Trie()
+        {
+        }
+
+        public void Insert(string word)
+        {
+            var node = root;
+            foreach (var character in word)
+            {
+                if (!node.children.ContainsKey(character))
+                {
+                    node.children[character] = new Node();
+                }
+                node = node.children[character];
+            }
+            node.isCompleteWord = true;
+        }
+
+        private Node FindNode(string word)
+        {
+            var node = root;
+            foreach (var character in word)
+            {
+                if (!node.children.ContainsKey(character))
+                {
+                    return null;
+                }
+                node = node.children[character];
+            }
+            return node;
+        }
+
+        public bool Search(string word)
+        {
+            return FindNode(word)?.isCompleteWord ?? false;
+        }
+
+        public bool StartsWith(string prefix)
+        {
+            return FindNode(prefix) != null;
+        }
+    }
+}
