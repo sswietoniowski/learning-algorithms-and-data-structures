a6b55775c89984516f4d0b5520e96d1a5ce0367c
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-08-31 08:06:54 +0200

    Solved "Diameter of Binary Tree"

--- neetcode/P543_DiametrOfBinaryTree/Solution.cs
+++ neetcode/P543_DiametrOfBinaryTree/Solution.cs
@@ -16,6 +16,31 @@
                 this.right = right;
             }
         }
-        
+
+        private int HeightOfBinaryTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return 1 + Math.Max(HeightOfBinaryTree(root.left), HeightOfBinaryTree(root.right));
+        }
+
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var leftHeight = HeightOfBinaryTree(root.left);
+            var rightHeight = HeightOfBinaryTree(root.right);
+
+            var leftDiameter = DiameterOfBinaryTree(root.left);
+            var rightDiameter = DiameterOfBinaryTree(root.right);
+
+            return Math.Max(Math.Max(leftDiameter, rightDiameter), leftHeight + rightHeight);
+        }
     }
 }
