fe7b5361e36960e18b1b4da58417375083865975
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-09-01 09:30:49 +0200

    Solved "Subsets"

--- neetcode/P0078_Subsets/Solution.cs
+++ neetcode/P0078_Subsets/Solution.cs
@@ -4,9 +4,27 @@
     // https://youtu.be/pV2kpPD66nE
     public class Solution
     {
+        private void dfs(int[] nums, IList<IList<int>> result, IList<int> subset, int i)
+        {
+            if (i >= nums.Length)
+            {
+                result.Add(new List<int>(subset));
+                return;
+            }
+
+            subset.Add(nums[i]);
+            dfs(nums, result, subset, i + 1);
+
+            subset.Remove(nums[i]);
+            dfs(nums, result, subset, i + 1);
+        }
+
         public IList<IList<int>> Subsets(int[] nums)
         {
-            throw new NotImplementedException();
+            IList<IList<int>> result = new List<IList<int>>();
+            IList<int> subset = new List<int>();
+            dfs(nums, result, subset, 0);
+            return result;
         }
     }
 }
