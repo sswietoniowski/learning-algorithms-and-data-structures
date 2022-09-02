8dd832e580b133f7aed430097637509df5d5f386
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-09-02 14:45:36 +0200

    Solved "Top K Frequent Elements"

--- neetcode/P0347_TopKFrequentElements/Solution.cs
+++ neetcode/P0347_TopKFrequentElements/Solution.cs
@@ -4,5 +4,34 @@
     // https://youtu.be/YPTqKIgVk-k
     public class Solution
     {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int> dict = new();
+            foreach (var num in nums)
+            {
+                if (dict.ContainsKey(num))
+                {
+                    dict[num]++;
+                }
+                else
+                {
+                    dict[num] = 1;
+                }
+            }
+
+            PriorityQueue<int, int> priorityQueue = new();
+            foreach (var (key, value) in dict)
+            {
+                priorityQueue.Enqueue(key, -value);
+            }
+
+            int[] result = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                result[i] = priorityQueue.Dequeue();
+            }
+
+            return result;
+        }
     }
 }
