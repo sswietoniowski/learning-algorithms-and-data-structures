ce919ae678f62a8f2d30718e024e42a4a0be9bb4
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-09-02 06:42:59 +0200

    Solved "Length of Last Word"

---
+++ leetcode/P0058_LengthOfLastWord/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace leetcode.P0058_LengthOfLastWord
+{
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            Regex re = new Regex(@"\s+");
+            string[] words = re.Split(s);
+            int i = words.Length - 1;
+            for (; i >= 0; i--)
+            {
+                if (!string.IsNullOrEmpty(words[i]))
+                {
+                    break;
+                }
+            }
+            return words[i].Length;
+        }
+    }
+}
