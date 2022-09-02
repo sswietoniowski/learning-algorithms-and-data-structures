f87b6e36b71bfba2df496062cc620054f6c60c03
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-09-02 14:38:38 +0200

    Solved "Maximum Subarray"

--- neetcode/P0053_MaximumSubarray/Solution.cs
+++ neetcode/P0053_MaximumSubarray/Solution.cs
@@ -6,7 +6,21 @@
     {
         public int MaxSubArray(int[] nums)
         {
-            throw new NotImplementedException();
+            int maxSum = nums[0];
+            int currentSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (currentSum < 0)
+                {
+                    currentSum = 0;
+                }
+                currentSum += nums[i];
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                }
+            }
+            return maxSum;
         }
     }
 }
