eb4f0e8ed527dca5e220e9de827e89cb34b090f7
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-04 16:41:59 +0200

    Solved "Counting Bits"

---
+++ neetcode/P0338_CountingBits/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace neetcode.P0338_CountingBits
+{
+    // https://leetcode.com/problems/counting-bits/
+    // https://youtu.be/RyBM56RIWrM
+    public class Solution
+    {
+        public int[] CountBits(int n)
+        {
+            var result = new int[n + 1];
+            for (var i = 1; i <= n; i++)
+            {
+                result[i] = result[i >> 1] + (i & 1);
+            }
+            return result;
+        }
+    }
+}
