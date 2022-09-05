5c49227043233501505b31708ec150efa682cfe5
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-05 08:39:01 +0200

    Solved "Unique Paths"

---
+++ neetcode/P0062_UniquePaths/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace neetcode.P0062_UniquePaths
+{
+    // https://leetcode.com/problems/unique-paths/
+    // https://youtu.be/IlEsdxuD4lY
+    public class Solution
+    {
+        public int UniquePaths(int m, int n)
+        {
+            int[] row = Enumerable.Repeat(1, n).ToArray();
+            for (int i = 0; i < m - 1; i++)
+            {
+                int[] newRow = Enumerable.Repeat(1, n).ToArray();
+                for (int j = n - 2; j >= 0; j--)
+                {
+                    newRow[j] = newRow[j + 1] + row[j];
+                }
+
+                row = newRow;
+            }
+
+            return row[0];
+        }
+    }
+}
