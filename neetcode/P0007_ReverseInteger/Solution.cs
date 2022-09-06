549f1f2ab480a93d35e1775ec660f9f2b2b7ec99
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-06 15:44:18 +0200

    Solved "Reverse Integer"

---
+++ neetcode/P0007_ReverseInteger/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace neetcode.P0007_ReverseInteger
+{
+    // https://leetcode.com/problems/reverse-integer/
+    // https://youtu.be/HAgLH58IgJQ
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            int result = 0;
+            int power = 1;
+            int sign = (x < 0 ? -1 : 1);
+            try
+            {
+                checked
+                {
+                    foreach (var c in Math.Abs(x).ToString())
+                    {
+                        result += sign * (c - '0') * power;
+                        unchecked
+                        {
+                            power *= 10;
+                        }
+                    }
+                }
+            }
+            catch (OverflowException e)
+            {
+                result = 0;
+            }
+            return result;
+        }
+    }
+}
