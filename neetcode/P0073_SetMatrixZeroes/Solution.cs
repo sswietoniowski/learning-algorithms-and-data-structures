9cd845d67bc23e6c9f83ebb565d1863c4b71a138
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-10 16:42:37 +0200

    Solved "Set Matrix Zeroes"

---
+++ neetcode/P0073_SetMatrixZeroes/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace neetcode.P0073_SetMatrixZeroes
+{
+    public class Solution
+    {
+        public void SetZeroes(int[][] matrix)
+        {
+            HashSet<int> zeroedRows = new();
+            HashSet<int> zeroedColumns = new();
+
+            int row = 0;
+            int column = 0;
+            int rowLength = matrix.Length;
+            int columnLength = matrix[0].Length;
+
+            while (row < rowLength && column < columnLength)
+            {
+                if (matrix[row][column] == 0)
+                {
+                    zeroedRows.Add(row);
+                    zeroedColumns.Add(column);
+                }
+
+                column = (column + 1) % columnLength;
+                row = column == 0 ? row + 1 : row;
+            }
+
+            row = 0;
+            column = 0;
+            while (row < rowLength && column < columnLength)
+            {
+                if (zeroedRows.Contains(row) || zeroedColumns.Contains(column))
+                {
+                    matrix[row][column] = 0;
+                }
+
+                column = (column + 1) % columnLength;
+                row = column == 0 ? row + 1 : row;
+            }
+        }
+    }
+}
--- neetcode/Program.cs
+++ neetcode/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello NeetCode");
+﻿using neetcode.P0073_SetMatrixZeroes;
+
+Console.WriteLine("Hello NeetCode");
+
+Solution solution = new Solution();
+
+solution.SetZeroes(new int[][] { new int[] { 1, 1, 1 }, new int[] { 1, 0, 1 }, new int[] { 1, 1, 1 } });
