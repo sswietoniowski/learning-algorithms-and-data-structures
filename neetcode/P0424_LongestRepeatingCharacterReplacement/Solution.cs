ff24d717fe0c5ab299d2ef97d35aa3c4e509d187
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-14 10:05:15 +0200

    Solved "Longest Repeating Character Replacement"

---
+++ neetcode/P0424_LongestRepeatingCharacterReplacement/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace neetcode.P0424_LongestRepeatingCharacterReplacement
+{
+    // https://leetcode.com/problems/longest-repeating-character-replacement/
+    // https://youtu.be/gqXU1UyA8pk
+    public class Solution
+    {
+        public int CharacterReplacement(string s, int k)
+        {
+            int result = 0;
+
+            int left = 0;
+
+            int[] charCount = new int[26];
+
+            for (int right = 0; right < s.Length; right++)
+            {
+                charCount[s[right] - 'A']++;
+
+                while (right - left + 1 - charCount.Max() > k)
+                {
+                    charCount[s[left] - 'A']--;
+                    left++;
+                }
+
+                result = Math.Max(result, right - left + 1);
+            }
+
+            return result;
+        }
+    }
+}
