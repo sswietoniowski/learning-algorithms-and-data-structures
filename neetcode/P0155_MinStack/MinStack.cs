ac4eff431c39eea9b8df90110bb5e7c9c67d2ab5
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-16 10:41:48 +0200

    Solved "Min Stack"

--- neetcode/P0155_MinStack/MinStack.cs
+++ neetcode/P0155_MinStack/MinStack.cs
@@ -1,31 +1,42 @@
 ﻿namespace neetcode.P0155_MinStack;
 
+// https://leetcode.com/problems/min-stack/
+// https://youtu.be/qkLl7nAwDPo
 public class MinStack
 {
-
+    private readonly Stack<int> _stack = new();
+    private readonly Stack<int> _minStack = new();
+    
     public MinStack()
     {
-
     }
 
     public void Push(int val)
     {
-
+        _stack.Push(val);
+        if (_minStack.Count == 0 || val <= _minStack.Peek())
+        {
+            _minStack.Push(val);
+        }
     }
 
     public void Pop()
     {
-
+        var val = _stack.Pop();
+        if (val == _minStack.Peek())
+        {
+            _minStack.Pop();
+        }
     }
 
     public int Top()
     {
-
+        return _stack.Peek();
     }
 
     public int GetMin()
     {
-
+        return _minStack.Peek();
     }
 }
 
