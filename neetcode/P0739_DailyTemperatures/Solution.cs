7a2b2e4e7db1afb5edbd7ad59afc8558c4e63c87
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-17 20:59:55 +0200

    Solved "Daily Temperatures"

---
+++ neetcode/P0739_DailyTemperatures/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace neetcode.P0739_DailyTemperatures
+{
+    // https://leetcode.com/problems/daily-temperatures/
+    // https://youtu.be/cTBiBSnjO3c
+    public class Solution
+    {
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            int[] result = new int[temperatures.Length];
+
+            for (int i = 0; i < temperatures.Length; i++)
+            {
+                int j = i + 1;
+                while (j < temperatures.Length)
+                {
+                    if (temperatures[j] > temperatures[i])
+                    {
+                        result[i] = j - i;
+                        break;
+                    }
+                    j++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
