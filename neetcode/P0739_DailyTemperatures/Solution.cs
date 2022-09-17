5a8157d2199ab5a1a4566b4820f0b3d08922006e
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-17 21:04:46 +0200

    Improved solution

--- neetcode/P0739_DailyTemperatures/Solution.cs
+++ neetcode/P0739_DailyTemperatures/Solution.cs
@@ -4,22 +4,44 @@
     // https://youtu.be/cTBiBSnjO3c
     public class Solution
     {
+        // v1
+        //public int[] DailyTemperatures(int[] temperatures)
+        //{
+        //    int[] result = new int[temperatures.Length];
+
+        //    for (int i = 0; i < temperatures.Length; i++)
+        //    {
+        //        int j = i + 1;
+        //        while (j < temperatures.Length)
+        //        {
+        //            if (temperatures[j] > temperatures[i])
+        //            {
+        //                result[i] = j - i;
+        //                break;
+        //            }
+        //            j++;
+        //        }
+        //    }
+
+        //    return result;
+        //}
+
+        // v2
         public int[] DailyTemperatures(int[] temperatures)
         {
             int[] result = new int[temperatures.Length];
 
+            Stack<int> stack = new();
+
             for (int i = 0; i < temperatures.Length; i++)
             {
-                int j = i + 1;
-                while (j < temperatures.Length)
+                while (stack.Count > 0 && temperatures[i] > temperatures[stack.Peek()])
                 {
-                    if (temperatures[j] > temperatures[i])
-                    {
-                        result[i] = j - i;
-                        break;
-                    }
-                    j++;
+                    int index = stack.Pop();
+                    result[index] = i - index;
                 }
+
+                stack.Push(i);
             }
 
             return result;
