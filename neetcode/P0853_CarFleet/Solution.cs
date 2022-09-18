763a5050521ed5b6f444dcb577c255253fd84b6a
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-18 08:44:13 +0200

    Solved "Car Fleet"

---
+++ neetcode/P0853_CarFleet/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace neetcode.P0853_CarFleet
+{
+    // https://leetcode.com/problems/car-fleet/
+    // https://youtu.be/Pr6T-3yB9RM
+    public class Solution
+    {
+        public int CarFleet(int target, int[] position, int[] speed)
+        {
+            (int, int)[] combined = position.Zip(speed, (p, s) => (p, s)).ToArray(); // join position & speed into tuple
+            Array.Sort(combined, (a, b) => b.Item1.CompareTo(a.Item1)); // sort descending
+
+            int fleet = 0;
+            double time = 0;
+            
+            foreach (var car in combined)
+            {
+                double t = (double)(target - car.Item1) / car.Item2;
+                if (t > time)
+                {
+                    fleet++;
+                    time = t;
+                }
+            }
+
+            return fleet;
+        }
+    }
+}
