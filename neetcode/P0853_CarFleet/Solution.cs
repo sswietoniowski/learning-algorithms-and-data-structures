8359245c8e79e8f15883ffcf8dd824b5a0b83651
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-18 08:49:26 +0200

    Improved solution

--- neetcode/P0853_CarFleet/Solution.cs
+++ neetcode/P0853_CarFleet/Solution.cs
@@ -4,25 +4,46 @@
     // https://youtu.be/Pr6T-3yB9RM
     public class Solution
     {
+        // v1
+        //public int CarFleet(int target, int[] position, int[] speed)
+        //{
+        //    (int, int)[] combined = position.Zip(speed, (p, s) => (p, s)).ToArray(); // join position & speed into tuple
+        //    Array.Sort(combined, (a, b) => b.Item1.CompareTo(a.Item1)); // sort descending
+
+        //    int fleet = 0;
+        //    double time = 0;
+
+        //    foreach (var car in combined)
+        //    {
+        //        double t = (double)(target - car.Item1) / car.Item2;
+        //        if (t > time)
+        //        {
+        //            fleet++;
+        //            time = t;
+        //        }
+        //    }
+
+        //    return fleet;
+        //}
+
+        // v2
         public int CarFleet(int target, int[] position, int[] speed)
         {
             (int, int)[] combined = position.Zip(speed, (p, s) => (p, s)).ToArray(); // join position & speed into tuple
             Array.Sort(combined, (a, b) => b.Item1.CompareTo(a.Item1)); // sort descending
 
-            int fleet = 0;
-            double time = 0;
-            
+            Stack<double> times = new();
+
             foreach (var car in combined)
             {
                 double t = (double)(target - car.Item1) / car.Item2;
-                if (t > time)
+                if (times.Count == 0 || t > times.Peek())
                 {
-                    fleet++;
-                    time = t;
+                    times.Push(t);
                 }
             }
 
-            return fleet;
+            return times.Count;
         }
     }
 }
