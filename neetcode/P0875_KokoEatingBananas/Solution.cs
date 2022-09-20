c435d50234850e8fc9fde937726f5ea49d1b186c
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-20 08:55:38 +0200

    Solved "Koko Eating Bananas"

---
+++ neetcode/P0875_KokoEatingBananas/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace neetcode.P0875_KokoEatingBananas
+{
+    // https://leetcode.com/problems/koko-eating-bananas/
+    // https://youtu.be/U2SozAs9RzA
+    public class Solution
+    {
+        public int MinEatingSpeed(int[] piles, int h)
+        {
+            int left = 1;
+            int right = int.MaxValue;
+
+            while (left < right)
+            {
+                int k = left + (right - left) / 2;
+                int hours = 0;
+
+                for (int i = 0; i < piles.Length; i++)
+                {
+                    hours += piles[i] / k + (piles[i] % k != 0 ? 1 : 0);
+                }
+
+                if (hours > h)
+                {
+                    left = k + 1;
+                }
+                else
+                {
+                    right = k;
+                }
+            }
+            return left;
+        }
+    }
+}
