3003f72cb1db7393c76a3204b9d348c46386a217
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-24 09:06:23 +0200

    Solved "Reorder List"

--- neetcode/P0143_ReorderList/Solution.cs
+++ neetcode/P0143_ReorderList/Solution.cs
@@ -23,10 +23,49 @@ public class ListNode
      * }
      */
 
+
+// https://leetcode.com/problems/reorder-list/
+// https://youtu.be/S5bfdUTrKLM
 public class Solution
 {
     public void ReorderList(ListNode head)
     {
+        if (head == null || head.next == null)
+        {
+            return;
+        }
 
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalf = slow.next;
+
+        slow.next = null;
+
+        ListNode previousNode = null;
+        while (secondHalf != null)
+        {
+            ListNode tmp = secondHalf.next;
+            secondHalf.next = previousNode;
+            previousNode = secondHalf;
+            secondHalf = tmp;
+        }
+
+        ListNode firstHalf = head;
+
+        while (firstHalf != null && previousNode != null)
+        {
+            ListNode tmp = firstHalf.next;
+            firstHalf.next = previousNode;
+            previousNode = previousNode.next;
+            firstHalf.next.next = tmp;
+            firstHalf = tmp;
+        }
     }
 }
