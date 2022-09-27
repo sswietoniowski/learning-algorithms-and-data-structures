c20413390788ebd8dc615e4f68da423386ac9903
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-27 08:23:13 +0200

    Solved "Add Two Numbers"

---
+++ neetcode/P0002_AddTwoNumbers/Solution.cs
@@ -0,0 +1,58 @@
+﻿namespace neetcode.P0002_AddTwoNumbers;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+// https://leetcode.com/problems/add-two-numbers/
+// https://youtu.be/wgFPrzTjm7s
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode head = new ListNode();
+        ListNode current = head;
+        int carry = 0;
+        while (l1 != null || l2 != null)
+        {
+            int sum = carry;
+            if (l1 != null)
+            {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null)
+            {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+        }
+        if (carry > 0)
+        {
+            current.next = new ListNode(carry);
+        }
+        return head.next;
+    }
+}
