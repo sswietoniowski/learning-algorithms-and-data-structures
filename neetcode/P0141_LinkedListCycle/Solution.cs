66c512756c8c86efe3b25181e426bd2545f3f1a0
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-28 07:52:16 +0200

    Solved "Linked List Cycle"

---
+++ neetcode/P0141_LinkedListCycle/Solution.cs
@@ -0,0 +1,49 @@
+﻿namespace neetcode.P0141_LinkedListCycle;
+
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+// https://leetcode.com/problems/linked-list-cycle/
+// https://youtu.be/gBTe7lFR3vc
+public class Solution
+{
+    public bool HasCycle(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
