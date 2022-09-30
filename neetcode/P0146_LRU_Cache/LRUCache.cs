d1c345c36769497ad9bb331d50d04ca274df39a0
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-30 07:31:49 +0200

    Code refactored, added starter code for a new problem

---
+++ neetcode/P0146_LRU_Cache/LRUCache.cs
@@ -0,0 +1,29 @@
+﻿namespace neetcode.P0146_LRU_Cache;
+
+// https://leetcode.com/problems/lru-cache/
+// https://youtu.be/7ABFKPK2hD4
+public class LRUCache
+{
+
+    public LRUCache(int capacity)
+    {
+
+    }
+
+    public int Get(int key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Put(int key, int value)
+    {
+
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.Get(key);
+ * obj.Put(key,value);
+ */
--- neetcode/P0287_FindTheDuplicateNumber/Solution.cs
+++ neetcode/P0287_FindTheDuplicateNumber/Solution.cs
@@ -1,28 +1,27 @@
-﻿namespace neetcode.P0287_FindTheDuplicateNumber
+﻿namespace neetcode.P0287_FindTheDuplicateNumber;
+
+public class Solution
 {
-    public class Solution
+    // https://leetcode.com/problems/find-the-duplicate-number/
+    // https://youtu.be/wjYnzkAhcNk
+    public int FindDuplicate(int[] nums)
     {
-        // https://leetcode.com/problems/find-the-duplicate-number/
-        // https://youtu.be/wjYnzkAhcNk
-        public int FindDuplicate(int[] nums)
+        int slow = 0;
+        int fast = 0;
+
+        do
         {
-            int slow = 0;
-            int fast = 0;
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
 
-            do
-            {
-                slow = nums[slow];
-                fast = nums[nums[fast]];
-            } while (slow != fast);
-
-            int slow2 = 0;
-            while (slow2 != slow)
-            {
-                slow2 = nums[slow2];
-                slow = nums[slow];
-            }
-
-            return slow;
+        int slow2 = 0;
+        while (slow2 != slow)
+        {
+            slow2 = nums[slow2];
+            slow = nums[slow];
         }
+
+        return slow;
     }
 }
