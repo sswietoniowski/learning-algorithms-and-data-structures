de347f3896ced2aa1494c50ebbe5bf1c67116703
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-10-05 07:56:10 +0200

    Solved "Binary Tree Level Order Traversal"

---
+++ neetcode/P0102_BinaryTreeLevelOrderTraversal/Solution.cs
@@ -0,0 +1,68 @@
+﻿namespace neetcode.P0102_BinaryTreeLevelOrderTraversal;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+// https://youtu.be/6ZnyEApgFYg
+public class Solution
+{
+    public IList<IList<int>> LevelOrder(TreeNode root)
+    {
+        var result = new List<IList<int>>();
+        if (root == null)
+        {
+            return result;
+        }
+
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        while (queue.Count > 0)
+        {
+            var level = new List<int>();
+            var levelSize = queue.Count;
+            for (var i = 0; i < levelSize; i++)
+            {
+                var node = queue.Dequeue();
+                level.Add(node.val);
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+            }
+
+            result.Add(level);
+        }
+
+        return result;
+    }
+}
