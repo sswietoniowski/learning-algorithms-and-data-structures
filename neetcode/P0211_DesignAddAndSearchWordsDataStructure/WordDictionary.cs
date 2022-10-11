4aa56a16a843e79e00a95641a1493a432522e607
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-10-11 12:37:54 +0200

    Solved "Design Add and Search Words Data Structure"

---
+++ neetcode/P0211_DesignAddAndSearchWordsDataStructure/WordDictionary.cs
@@ -0,0 +1,74 @@
+﻿namespace neetcode.P0211_DesignAddAndSearchWordsDataStructure;
+
+// https://leetcode.com/problems/design-add-and-search-words-data-structure/
+// https://youtu.be/BTf05gs_8iU
+public class WordDictionary
+{
+    private class Node
+    {
+        public Dictionary<char, Node> Children { get; } = new();
+        public bool IsCompleteWord { get; set; } = false;
+    }
+
+    private Node root = new();
+
+    public WordDictionary() {}
+
+    public void AddWord(string word)
+    {
+        var node = root;
+        foreach (var character in word)
+        {
+            if (!node.Children.ContainsKey(character))
+            {
+                node.Children[character] = new Node();
+            }
+            node = node.Children[character];
+        }
+        node.IsCompleteWord = true;
+    }
+
+    private bool dfs(Node root, string word, int index)
+    {
+        var node = root;
+
+        for (int i = index; i < word.Length; i++)
+        {
+            var character = word[i];
+            if (character == '.')
+            {
+                foreach (var child in node.Children.Values)
+                {
+                    if (dfs(child, word, i + 1))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+            else
+            {
+                if (!node.Children.ContainsKey(character))
+                {
+                    return false;
+                }
+                node = node.Children[character];
+            }
+        }
+
+        return node is { IsCompleteWord: true };
+    }
+
+    public bool Search(string word)
+    {
+        return dfs(root, word, 0);
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.AddWord(word);
+ * bool param_2 = obj.Search(word);
+ */
