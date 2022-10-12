46735349a61de6d68a58299ddb2af92be2e03b55
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-10-12 09:22:31 +0200

    Solved "K Closest Points to Origin"

--- neetcode/P0973_KClosestPointsToOrigin/Solution.cs
+++ neetcode/P0973_KClosestPointsToOrigin/Solution.cs
@@ -6,6 +6,21 @@ public class Solution
 {
     public int[][] KClosest(int[][] points, int k)
     {
-        throw new NotImplementedException();
+        var heap = new PriorityQueue<int[], int>();
+
+        foreach (var point in points)
+        {
+            var distance = point[0] * point[0] + point[1] * point[1];
+            heap.Enqueue(point, distance);
+        }
+
+        var result = new int[k][];
+
+        for (var i = 0; i < k; i++)
+        {
+            result[i] = heap.Dequeue();
+        }
+
+        return result;
     }
 }
