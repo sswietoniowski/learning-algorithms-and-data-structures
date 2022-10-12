b9f9dd57d8365e680ef6094bf154c31a96a698f3
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-10-12 07:57:27 +0200

    Added starter code

---
+++ neetcode/P0973_KClosestPointsToOrigin/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace neetcode.P0973_KClosestPointsToOrigin;
+
+// https://leetcode.com/problems/k-closest-points-to-origin/
+// https://youtu.be/rI2EBUEMfTk
+public class Solution
+{
+    public int[][] KClosest(int[][] points, int k)
+    {
+        throw new NotImplementedException();
+    }
+}
