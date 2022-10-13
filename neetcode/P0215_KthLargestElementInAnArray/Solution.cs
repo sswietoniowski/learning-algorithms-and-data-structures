54169ed6f4da86fa2276a875eeb63afe19d3f637
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-10-13 08:21:16 +0200

    Solved "Kth Largest Element in an Array"

---
+++ neetcode/P0215_KthLargestElementInAnArray/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace neetcode.P0215_KthLargestElementInAnArray;
+
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+// https://youtu.be/XEmy13g1Qxc
+public class Solution
+{
+    public int FindKthLargest(int[] nums, int k)
+    {
+        var heap = new PriorityQueue<int, int>();
+
+        foreach (var num in nums)
+        {
+            heap.Enqueue(num, -num);
+        }
+
+        for (int i = 0; i < k - 1; i++)
+        {
+            heap.Dequeue();
+        }
+
+        return heap.Dequeue();
+    }
+}
