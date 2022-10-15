d5a6cee46d16ecdf442d5190f01cbee64fc3909d
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-10-15 07:50:20 +0200

    Created starter code

---
+++ neetcode/P0355_DesignTwitter/Twitter.cs
@@ -0,0 +1,41 @@
+﻿namespace neetcode.P0355_DesignTwitter;
+
+// https://leetcode.com/problems/design-twitter/
+// https://youtu.be/pNichitDD2E
+public class Twitter
+{
+
+    public Twitter()
+    {
+
+    }
+
+    public void PostTweet(int userId, int tweetId)
+    {
+
+    }
+
+    public IList<int> GetNewsFeed(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Follow(int followerId, int followeeId)
+    {
+
+    }
+
+    public void Unfollow(int followerId, int followeeId)
+    {
+
+    }
+}
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.PostTweet(userId,tweetId);
+ * IList<int> param_2 = obj.GetNewsFeed(userId);
+ * obj.Follow(followerId,followeeId);
+ * obj.Unfollow(followerId,followeeId);
+ */
