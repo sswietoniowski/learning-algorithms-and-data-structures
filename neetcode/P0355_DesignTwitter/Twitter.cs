b8a3a2e266fdf255c1a4ff8dc27316eeb88e67c5
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-10-15 08:56:46 +0200

    Solved "Design Twitter"

--- neetcode/P0355_DesignTwitter/Twitter.cs
+++ neetcode/P0355_DesignTwitter/Twitter.cs
@@ -4,30 +4,94 @@
 // https://youtu.be/pNichitDD2E
 public class Twitter
 {
+    private static int count = 0;
+    private readonly Dictionary<int, HashSet<int>> _follows;
+    private readonly Dictionary<int, List<(int, int)>> _tweets;
 
     public Twitter()
     {
-
+        _follows = new Dictionary<int, HashSet<int>>();
+        _tweets = new Dictionary<int, List<(int, int)>>();
     }
 
     public void PostTweet(int userId, int tweetId)
     {
+        if (!_tweets.ContainsKey(userId))
+        {
+            _tweets.Add(userId, new List<(int, int)>());
+        }
 
+        count++;
+        _tweets[userId].Add((tweetId, count));
     }
 
     public IList<int> GetNewsFeed(int userId)
     {
-        throw new NotImplementedException();
+        var result = new List<int>();
+
+        var maxHeap = new PriorityQueue<int, int>();
+
+        if (_follows.ContainsKey(userId))
+        {
+            foreach (var followee in _follows[userId])
+            {
+                if (_tweets.ContainsKey(followee))
+                {
+                    int index = _tweets[followee].Count - 1;
+                    int counter = 0;
+
+                    while (index >= 0 && counter < 10)
+                    {
+                        var (tweetId, count) = _tweets[followee][index];
+                        maxHeap.Enqueue(tweetId, -count);
+                        index--;
+                        counter++;
+                    }
+                }
+            }
+        }
+
+        if (_tweets.ContainsKey(userId))
+        {
+            int index = _tweets[userId].Count - 1;
+            int counter = 0;
+            
+            while (index >= 0 && counter < 10)
+            {
+                var (tweetId, count) = _tweets[userId][index];
+                maxHeap.Enqueue(tweetId, -count);
+                index--;
+                counter++;
+            }
+        }
+
+        int i = 0;
+        while (i < 10 && maxHeap.Count > 0)
+        {
+            var tweetId = maxHeap.Dequeue();
+            result.Add(tweetId);
+            i++;
+        }
+
+        return result;
     }
 
     public void Follow(int followerId, int followeeId)
     {
+        if (!_follows.ContainsKey(followerId))
+        {
+            _follows.Add(followerId, new HashSet<int>());
+        }
 
+        _follows[followerId].Add(followeeId);
     }
 
     public void Unfollow(int followerId, int followeeId)
     {
-
+        if (_follows.ContainsKey(followerId))
+        {
+            _follows[followerId].Remove(followeeId);
+        }
     }
 }
 
