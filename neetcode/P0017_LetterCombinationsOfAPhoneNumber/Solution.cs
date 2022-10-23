8e0ee555d32b50538e3576a35bb0758b104d3413
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-10-23 08:56:53 +0200

    Solved "Letter Combinations of a Phone Number"

---
+++ neetcode/P0017_LetterCombinationsOfAPhoneNumber/Solution.cs
@@ -0,0 +1,53 @@
+﻿namespace neetcode.P0017_LetterCombinationsOfAPhoneNumber;
+
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+// https://youtu.be/0snEunUacZY
+public class Solution
+{
+    public IList<string> LetterCombinations(string digits)
+    {
+        var result = new List<string>();
+
+        if (digits.Length == 0)
+        {
+            return result;
+        }
+
+        var map = new Dictionary<char, string>()
+        {
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"}
+        };
+
+        var queue = new Queue<string>();
+
+        queue.Enqueue("");
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+
+            if (current.Length == digits.Length)
+            {
+                result.Add(current);
+            }
+            else
+            {
+                var letters = map[digits[current.Length]];
+
+                foreach (var letter in letters)
+                {
+                    queue.Enqueue(current + letter);
+                }
+            }
+        }
+
+        return result;
+    }
+}
