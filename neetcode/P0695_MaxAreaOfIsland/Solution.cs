2419a99d351eda1ab491f5fa4b6ebe04a7825fcd
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-10-25 10:07:31 +0200

    Solved "Max Area of Island"

--- neetcode/P0695_MaxAreaOfIsland/Solution.cs
+++ neetcode/P0695_MaxAreaOfIsland/Solution.cs
@@ -4,8 +4,31 @@
 // https://youtu.be/iJGr1OtmH0c
 public class Solution
 {
+    private int GetArea(int[][] grid, int row, int col)
+    {
+        if (row < 0 || row >= grid.Length || col < 0 || col >= grid[row].Length || grid[row][col] == 0)
+        {
+            return 0;
+        }
+
+        grid[row][col] = 0;
+        return 1 + GetArea(grid, row - 1, col) + GetArea(grid, row + 1, col) + GetArea(grid, row, col - 1) + GetArea(grid, row, col + 1);
+    }
+
     public int MaxAreaOfIsland(int[][] grid)
     {
-        throw new NotImplementedException();
+        int maxArea = 0;
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[i].Length; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    maxArea = Math.Max(maxArea, GetArea(grid, i, j));
+                }
+            }
+        }
+
+        return maxArea;
     }
 }
