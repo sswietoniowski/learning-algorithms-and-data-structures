3c46b939e57726deaed25939dd71cfa5bbd07345
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-10-25 08:53:22 +0200

    Added starter code

---
+++ neetcode/P0695_MaxAreaOfIsland/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace neetcode.P0695_MaxAreaOfIsland;
+
+// https://leetcode.com/problems/max-area-of-island/
+// https://youtu.be/iJGr1OtmH0c
+public class Solution
+{
+    public int MaxAreaOfIsland(int[][] grid)
+    {
+        throw new NotImplementedException();
+    }
+}
