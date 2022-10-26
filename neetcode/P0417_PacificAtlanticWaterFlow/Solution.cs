9cb46de6fc5e0cdb749c817502561f19653fc3e2
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-10-26 07:58:43 +0200

    Added starter code

---
+++ neetcode/P0417_PacificAtlanticWaterFlow/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace neetcode.P0417_PacificAtlanticWaterFlow;
+
+// https://leetcode.com/problems/pacific-atlantic-water-flow/
+// https://youtu.be/s-VkcjHqkGI
+public class Solution
+{
+    public IList<IList<int>> PacificAtlantic(int[][] heights)
+    {
+        throw new NotImplementedException();
+    }
+}
