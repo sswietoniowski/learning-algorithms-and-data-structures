b39838c0dc5a1885e8e7edca7a9b85aa2af0f6ef
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-10-28 09:23:38 +0200

    Solved "Pacific Atlantic Water Flow"

--- neetcode/P0417_PacificAtlanticWaterFlow/Solution.cs
+++ neetcode/P0417_PacificAtlanticWaterFlow/Solution.cs
@@ -6,6 +6,51 @@ public class Solution
 {
     public IList<IList<int>> PacificAtlantic(int[][] heights)
     {
-        throw new NotImplementedException();
+        int rows = heights.Length;
+        int columns = heights[0].Length;
+
+        var pacific = new HashSet<(int, int)>();
+        var atlantic = new HashSet<(int, int)>();
+
+        void dfs(int row, int column, HashSet<(int, int)> visited, int previousHeight)
+        {
+            if (row < 0 || row >= rows || column < 0 || column >= columns 
+                || visited.Contains((row, column)) 
+                || heights[row][column] < previousHeight)
+            {
+                return;
+            }
+
+            visited.Add((row, column));
+
+            dfs(row + 1, column, visited, heights[row][column]);
+            dfs(row - 1, column, visited, heights[row][column]);
+            dfs(row, column + 1, visited, heights[row][column]);
+            dfs(row, column - 1, visited, heights[row][column]);
+        }
+
+        for (int i = 0; i < columns; i++)
+        {
+            dfs(0, i, pacific, heights[0][i]);
+            dfs(rows - 1, i, atlantic, heights[rows - 1][i]);
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            dfs(i, 0, pacific, heights[i][0]);
+            dfs(i, columns - 1, atlantic, heights[i][columns - 1]);
+        }
+
+        var result = new List<IList<int>>();
+
+        foreach (var item in pacific)
+        {
+            if (atlantic.Contains(item))
+            {
+                result.Add(new List<int> { item.Item1, item.Item2 });
+            }
+        }
+
+        return result;
     }
 }
