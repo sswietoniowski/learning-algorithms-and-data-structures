14aad7a31c2ba76ba5fac09f19c060af1591f2e0
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-11-07 07:37:25 +0100

    Refactored + created starter code

---
+++ neetcode/P0207_CourseSchedule/Solution.cs
@@ -0,0 +1,58 @@
+﻿namespace neetcode.P0207_CourseSchedule;
+
+// https://leetcode.com/problems/course-schedule/
+// https://youtu.be/e69C6xhiSQE
+public class Solution
+{
+    private bool IsCyclic(List<int>[] graph, int course, bool[] visited)
+    {
+        if (visited[course])
+        {
+            return true;
+        }
+
+        visited[course] = true;
+
+        foreach (var nextCourse in graph[course])
+        {
+            if (IsCyclic(graph, nextCourse, visited))
+            {
+                return true;
+            }
+        }
+
+        visited[course] = false;
+
+        return false;
+    }
+
+    public bool CanFinish(int numCourses, int[][] prerequisites)
+    {
+        var graph = new List<int>[numCourses];
+
+        for (int i = 0; i < numCourses; i++)
+        {
+            graph[i] = new List<int>();
+        }
+
+        foreach (var edge in prerequisites)
+        {
+            graph[edge[0]].Add(edge[1]);
+        }
+
+        var visited = new bool[numCourses];
+
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (visited[i] == false)
+            {
+                if (IsCyclic(graph, i, visited))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
