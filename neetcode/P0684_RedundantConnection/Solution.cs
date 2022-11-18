2716c388c5696d8d31ed50ea1ea3410aefb0bce6
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-11-18 15:35:21 +0100

    Solved "Redundant Connection"

--- neetcode/P0684_RedundantConnection/Solution.cs
+++ neetcode/P0684_RedundantConnection/Solution.cs
@@ -6,6 +6,56 @@ public class Solution
 {
     public int[] FindRedundantConnection(int[][] edges)
     {
-        throw new NotImplementedException();
+        var parent = new int[edges.Length + 1];
+        for (int i = 0; i < parent.Length; i++)
+        {
+            parent[i] = i;
+        }
+
+        var rank = new int[edges.Length + 1];
+        for (int i = 0; i < rank.Length; i++)
+        {
+            rank[i] = 1;
+        }
+
+        int Find(int x)
+        {
+            if (x != parent[x])
+            {
+                parent[x] = Find(parent[x]);
+            }
+
+            return parent[x];
+        }
+
+        foreach (var edge in edges)
+        {
+            var u = edge[0];
+            var v = edge[1];
+
+            var pu = Find(u);
+            var pv = Find(v);
+
+            if (pu == pv)
+            {
+                return edge;
+            }
+
+            if (rank[pu] < rank[pv])
+            {
+                parent[pu] = pv;
+            }
+            else if (rank[pu] > rank[pv])
+            {
+                parent[pv] = pu;
+            }
+            else
+            {
+                parent[pu] = pv;
+                rank[pv]++;
+            }
+        }
+
+        return null;
     }
 }
