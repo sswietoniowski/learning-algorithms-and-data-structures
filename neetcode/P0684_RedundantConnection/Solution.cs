96bd799502550eaf4897987cd8025cd010b2a2fc
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-11-18 15:00:34 +0100

    Added starter code

---
+++ neetcode/P0684_RedundantConnection/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace neetcode.P0684_RedundantConnection;
+
+// https://leetcode.com/problems/redundant-connection/
+// https://youtu.be/FXWRE67PLL0
+public class Solution
+{
+    public int[] FindRedundantConnection(int[][] edges)
+    {
+        throw new NotImplementedException();
+    }
+}
