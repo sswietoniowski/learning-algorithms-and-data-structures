9f5fc651589bb7ba5d42ddd4bd50fa582e10353f
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-12-10 22:20:38 +0100

    Solved problem

---
+++ neetcode/P0746_MinCostClimbingStairs/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace neetcode.P0746_MinCostClimbingStairs;
+
+// https://leetcode.com/problems/min-cost-climbing-stairs/
+// https://youtu.be/ktmzAZWkEZ0
+public class Solution
+{
+    public int MinCostClimbingStairs(int[] cost)
+    {
+        throw new NotImplementedException();
+    }
+}
