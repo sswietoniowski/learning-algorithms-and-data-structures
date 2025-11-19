3af4f6c5f7fa90c93040fe5d45ebec60e854d462
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2025-11-19 12:57:38 +0100

    feat: add solution to the LeetCode problem #124

---
+++ neetcode/P0124_BinaryTreeMaximumPathSum/Solution.cs
@@ -0,0 +1,53 @@
+﻿namespace neetcode.P0124_BinaryTreeMaximumPathSum;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution
+{
+    public int MaxPathSum(TreeNode root)
+    {
+        int res = root.val;
+        Dfs(root, ref res);
+        return res;
+    }
+
+    private int Dfs(TreeNode root, ref int res)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        int leftMax = Math.Max(Dfs(root.left, ref res), 0);
+        int rightMax = Math.Max(Dfs(root.right, ref res), 0);
+
+        res = Math.Max(res, root.val + leftMax + rightMax);
+        return root.val + Math.Max(leftMax, rightMax);
+    }
+}
