81a368688aea4e92e3706e183a33e3aa45fcb6ce
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2025-12-01 10:45:19 +0100

    feat: add solution to the LeetCode problem #55

---
+++ neetcode/P0055_JumpGame/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace neetcode.P0055_JumpGame;
+
+// https://leetcode.com/problems/jump-game/description/
+// https://neetcode.io/problems/jump-game/question?list=blind75
+public class Solution
+{
+    public bool CanJump(int[] nums)
+    {
+        int goal = nums.Length - 1;
+
+        for (int i = nums.Length - 2; i >= 0; i--)
+        {
+            if (i + nums[i] >= goal)
+            {
+                goal = i;
+            }
+        }
+
+        return goal == 0;
+    }
+}
