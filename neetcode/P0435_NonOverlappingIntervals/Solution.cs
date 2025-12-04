3923178c071bbb46867c472991797829be0b0825
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2025-12-04 09:19:50 +0100

    feat: add solution to the LeetCode problem #435

---
+++ neetcode/P0435_NonOverlappingIntervals/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace neetcode.P0435_NonOverlappingIntervals;
+
+// https://leetcode.com/problems/non-overlapping-intervals/description/
+// https://neetcode.io/problems/non-overlapping-intervals/question?list=blind75
+public class Solution
+{
+    public int EraseOverlapIntervals(int[][] intervals)
+    {
+        Array.Sort(intervals, (a, b) => a[1].CompareTo(b[1]));
+        int res = 0;
+        int prevEnd = intervals[0][1];
+
+        for (int i = 1; i < intervals.Length; i++)
+        {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            if (start < prevEnd)
+            {
+                res++;
+            }
+            else
+            {
+                prevEnd = end;
+            }
+        }
+        return res;
+    }
+}
