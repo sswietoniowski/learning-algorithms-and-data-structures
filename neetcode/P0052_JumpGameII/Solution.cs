2dfb8c25c52511fba8b2dcb3ef8fdaa2a40b1067
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2025-12-05 08:20:19 +0100

    feat: add solutions to the LeetCode problems

---
+++ neetcode/P0052_JumpGameII/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace neetcode.P0052_JumpGameII;
+
+// https://leetcode.com/problems/jump-game-ii/description/
+// https://neetcode.io/problems/jump-game-ii/question?list=neetcode150
+public class Solution
+{
+    public int Jump(int[] nums)
+    {
+        int res = 0,
+            l = 0,
+            r = 0;
+
+        while (r < nums.Length - 1)
+        {
+            int farthest = 0;
+            for (int i = l; i <= r; i++)
+            {
+                farthest = Math.Max(farthest, i + nums[i]);
+            }
+            l = r + 1;
+            r = farthest;
+            res++;
+        }
+        return res;
+    }
+}
---
+++ neetcode/P0253_MeetingRoomsII/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+
+namespace neetcode.P0253_MeetingRoomsII;
+
+public class Interval
+{
+    public int start,
+        end;
+
+    public Interval(int start, int end)
+    {
+        this.start = start;
+        this.end = end;
+    }
+}
+
+// https://leetcode.com/problems/meeting-rooms-ii
+// https://neetcode.io/problems/meeting-schedule-ii/question?list=blind75
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+public class Solution
+{
+    public int MinMeetingRooms(List<Interval> intervals)
+    {
+        List<int[]> time = new List<int[]>();
+        foreach (var i in intervals)
+        {
+            time.Add(new int[] { i.start, 1 });
+            time.Add(new int[] { i.end, -1 });
+        }
+
+        time.Sort((a, b) => a[0] == b[0] ? a[1].CompareTo(b[1]) : a[0].CompareTo(b[0]));
+
+        int res = 0,
+            count = 0;
+        foreach (var t in time)
+        {
+            count += t[1];
+            res = Math.Max(res, count);
+        }
+        return res;
+    }
+}
