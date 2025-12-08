cc8e3f7840362de44782c894cfb0eba9b3b3ec3e
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2025-12-08 10:49:15 +0100

    feat: add solution to the LeetCode problem #134

---
+++ neetcode/P0134_GasStation/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace neetcode.P0134_GasStation;
+
+// https://leetcode.com/problems/gas-station/description/
+// https://neetcode.io/problems/gas-station/question?list=neetcode150
+public class Solution
+{
+    public int CanCompleteCircuit(int[] gas, int[] cost)
+    {
+        int n = gas.Length;
+        int start = n - 1,
+            end = 0;
+        int tank = gas[start] - cost[start];
+        while (start > end)
+        {
+            if (tank < 0)
+            {
+                start--;
+                tank += gas[start] - cost[start];
+            }
+            else
+            {
+                tank += gas[end] - cost[end];
+                end++;
+            }
+        }
+        return tank >= 0 ? start : -1;
+    }
+}
