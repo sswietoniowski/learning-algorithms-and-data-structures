08e0f391616b059a6aa612523c4e155eea1c9ae1
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2025-12-14 08:08:31 +0100

    feat: add solutions to the LeetCode problems

---
+++ neetcode/P0518_CoinChangeII/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace neetcode.P0518_CoinChangeII;
+
+// https://leetcode.com/problems/coin-change-ii/
+// https://neetcode.io/problems/coin-change-ii/question
+public class Solution
+{
+    public int Change(int amount, int[] coins)
+    {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int i = coins.Length - 1; i >= 0; i--)
+        {
+            for (int a = 1; a <= amount; a++)
+            {
+                dp[a] += (coins[i] <= a ? dp[a - coins[i]] : 0);
+            }
+        }
+        return dp[amount];
+    }
+}
