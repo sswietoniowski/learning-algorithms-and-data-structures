63b27a45b61abadfe64d7d1b9e9ab28b03e7895d
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2025-12-19 09:05:04 +0100

    feat: add solutions to the LeetCode problems

---
+++ neetcode/P0072_EditDistance/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace neetcode.P0072_EditDistance;
+
+public class Solution
+{
+    public int MinDistance(string word1, string word2)
+    {
+        int m = word1.Length,
+            n = word2.Length;
+        if (m < n)
+        {
+            string temp = word1;
+            word1 = word2;
+            word2 = temp;
+            m = word1.Length;
+            n = word2.Length;
+        }
+
+        int[] dp = new int[n + 1];
+        for (int i = 0; i <= n; i++)
+            dp[i] = n - i;
+
+        for (int i = m - 1; i >= 0; i--)
+        {
+            int nextDp = dp[n];
+            dp[n] = m - i;
+            for (int j = n - 1; j >= 0; j--)
+            {
+                int temp = dp[j];
+                if (word1[i] == word2[j])
+                {
+                    dp[j] = nextDp;
+                }
+                else
+                {
+                    dp[j] = 1 + Math.Min(dp[j], Math.Min(dp[j + 1], nextDp));
+                }
+                nextDp = temp;
+            }
+        }
+        return dp[0];
+    }
+}
