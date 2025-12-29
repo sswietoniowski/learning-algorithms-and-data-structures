c31a9cd746e4f0bf8119484f49e93acc707f8d36
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2025-12-29 10:03:56 +0100

    feat: add solution for the "Swim in Rising Water" problem

---
+++ neetcode/P3110_ScoreOfAString/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace neetcode.P3110_ScoreOfAString;
+
+// https://leetcode.com/problems/score-of-a-string/description/
+// https://neetcode.io/problems/score-of-a-string/question
+public class Solution
+{
+    public int ScoreOfString(string s)
+    {
+        int sum = 0;
+        for (int i = 1; i < s.Length; i++)
+        {
+            int value = (int)s[i - 1] - (int)s[i];
+            sum += value >= 0 ? value : -value;
+        }
+        return sum;
+    }
+}
