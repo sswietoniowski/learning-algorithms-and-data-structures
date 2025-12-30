9bb4d7ec02457e77003dbbb714a1ef5a0512447c
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2025-12-30 12:07:53 +0100

    feat: add solution for the "Concatenation of Array" problem

---
+++ neetcode/P1929_ConcatenationOfArray/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace neetcode.P1929_ConcatenationOfArray;
+
+// https://leetcode.com/problems/concatenation-of-array/description/
+// https://neetcode.io/problems/concatenation-of-array/question?list=allNC
+public class Solution
+{
+    public int[] GetConcatenation(int[] nums)
+    {
+        var n = nums.Length;
+        var ans = new int[n * 2];
+
+        for (var i = 0; i < n; i++)
+        {
+            ans[i] = nums[i];
+            ans[i + n] = ans[i] = nums[i];
+        }
+
+        return ans;
+    }
+}
