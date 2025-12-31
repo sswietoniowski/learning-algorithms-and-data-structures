4b0661d4472d8aa5c0d0a284420c631309466f1d
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2025-12-31 07:39:58 +0100

    feat: add solution for the "Replace Elements with Greatest Element on Right Side" problem

---
+++ neetcode/P1299_ReplaceElements/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace neetcode.P1299_ReplaceElements;
+
+// https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/description/
+// https://neetcode.io/problems/replace-elements-with-greatest-element-on-right-side/question
+public class Solution
+{
+    public int[] ReplaceElements(int[] arr)
+    {
+        int n = arr.Length;
+        int[] ans = new int[n];
+        int max = arr[n - 1];
+
+        ans[n - 1] = -1;
+        for (int i = n - 2; i >= 0; i--)
+        {
+            ans[i] = max;
+            max = Math.Max(max, arr[i]);
+        }
+
+        return ans;
+    }
+}
