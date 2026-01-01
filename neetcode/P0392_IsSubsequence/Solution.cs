8a60ae0f45a9f7d269e342965211ac26986844c3
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-01 15:40:53 +0100

    feat: add solution for the "Is Subsequence" problem

---
+++ neetcode/P0392_IsSubsequence/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace neetcode.P0392_IsSubsequence;
+
+// https://leetcode.com/problems/is-subsequence/description/
+// https://neetcode.io/problems/is-subsequence/question
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        if (s.Length == 0)
+        {
+            return true;
+        }
+
+        if (s.Length > t.Length)
+        {
+            return false;
+        }
+
+        for (int i = 0, j = 0; i < s.Length && j < t.Length; j++)
+        {
+            if (s[i] == t[j])
+            {
+                i++;
+
+                if (i == s.Length)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
