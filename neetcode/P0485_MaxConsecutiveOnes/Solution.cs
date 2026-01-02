4fae290b41c66f7bd01889a5a2c9fce4e6a87d4b
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-02 15:06:50 +0100

    feat: add solution for the "Max Consecutive Ones" problem

---
+++ neetcode/P0485_MaxConsecutiveOnes/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace neetcode.P0485_MaxConsecutiveOnes;
+
+// https://leetcode.com/problems/max-consecutive-ones/description/
+// https://neetcode.io/problems/max-consecutive-ones/question
+public class Solution
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        int max = 0;
+        for (int i = 0, counter = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 1)
+            {
+                counter++;
+                if (counter > max)
+                {
+                    max = counter;
+                }
+            }
+            else
+            {
+                counter = 0;
+            }
+        }
+        return max;
+    }
+}
