93f5a3361ad08af49f583a0ce319f79454d4bee2
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-03 21:42:57 +0100

    feat: add solution for the "Append Characters to String to Make Subsequence" problem

---
+++ neetcode/P2486_AppendCharacters/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace neetcode.P2486_AppendCharacters;
+
+// https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/description/
+// https://neetcode.io/problems/append-characters-to-string-to-make-subsequence/question?list=allNC
+public class Solution
+{
+    public int AppendCharacters(string s, string t)
+    {
+        var j = 0;
+        for (int i = 0; i < s.Length && j < t.Length; i++)
+        {
+            if (s[i] == t[j])
+            {
+                j++;
+            }
+        }
+
+        return t.Length - j;
+    }
+}
