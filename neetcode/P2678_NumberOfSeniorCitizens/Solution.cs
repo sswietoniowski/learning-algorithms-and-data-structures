57d2dc818a905a3c48430c228e8c84db5eccc0fc
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-04 08:37:41 +0100

    feat: add solution for the "Number of Senior Citizens" problem

---
+++ neetcode/P2678_NumberOfSeniorCitizens/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace neetcode.P2678_NumberOfSeniorCitizens;
+
+// https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/description/
+// https://neetcode.io/problems/append-characters-to-string-to-make-subsequence/question?list=allNC
+public class Solution
+{
+    public int CountSeniors(string[] details)
+    {
+        var seniorCount = 0;
+        foreach (string entry in details)
+        {
+            ReadOnlySpan<char> ageSpan = entry.AsSpan(11, 2);
+            int age = (entry[11] - '0') * 10 + (entry[12] - '0');
+            if (age > 60)
+            {
+                seniorCount++;
+            }
+        }
+        return seniorCount;
+    }
+}
