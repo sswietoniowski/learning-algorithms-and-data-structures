4ede88c02424f9b2a7bd54429f3022625e4e4b62
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-05 12:03:11 +0100

    feat: add solution for the "String Matching in an Array" problem

---
+++ neetcode/P1408_StringMatchingInAnArray/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace neetcode.P1408_StringMatchingInAnArray;
+
+// https://leetcode.com/problems/string-matching-in-an-array/description/
+// https://neetcode.io/problems/string-matching-in-an-array/question?list=allNC
+public class Solution
+{
+    public List<string> StringMatching(string[] words)
+    {
+        HashSet<string> substrings = new();
+        for (int i = 0; i < words.Length; i++)
+        {
+            for (int j = 0; j < words.Length && i != j; j++)
+            {
+                if (words[i].Contains(words[j]))
+                {
+                    substrings.Add(words[j]);
+                }
+                else if (words[j].Contains(words[i]))
+                {
+                    substrings.Add(words[i]);
+                }
+            }
+        }
+        return substrings.ToList();
+    }
+}
