9e69e5ea89199a434340e6be1943c80ab1156be5
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-07 12:18:28 +0100

    feat: add solution for the "Isomorphic Strings" problem

---
+++ neetcode/P0205_IsomorphicStrings/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace neetcode.P0205_IsomorphicStrings;
+
+// https://leetcode.com/problems/isomorphic-strings/
+// https://neetcode.io/problems/isomorphic-strings/question?list=allNC
+public class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (mapS[s[i]] != mapT[t[i]])
+            {
+                return false;
+            }
+
+            mapS[s[i]] = i + 1;
+            mapT[t[i]] = i + 1;
+        }
+
+        return true;
+    }
+}
