f87bb44ac36eb4985d48d80538ee5ebb2735874c
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-08 09:26:51 +0100

    feat: add solution for the "Unique Email Addresses" problem

---
+++ neetcode/P0929_UniqueEmailAddresses/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace neetcode.P0929_UniqueEmailAddresses;
+
+// https://leetcode.com/problems/unique-email-addresses/description/
+// https://neetcode.io/problems/unique-email-addresses/question
+public class Solution
+{
+    public int NumUniqueEmails(string[] emails)
+    {
+        var uniqueEmails = new HashSet<string>();
+        var sb = new StringBuilder();
+
+        foreach (string email in emails)
+        {
+            sb.Clear();
+            int atIndex = email.IndexOf('@');
+
+            for (int i = 0; i < atIndex; i++)
+            {
+                char c = email[i];
+                if (c == '+')
+                    break;
+                if (c == '.')
+                    continue;
+                sb.Append(c);
+            }
+
+            sb.Append(email.AsSpan(atIndex));
+
+            uniqueEmails.Add(sb.ToString());
+        }
+
+        return uniqueEmails.Count;
+    }
+}
