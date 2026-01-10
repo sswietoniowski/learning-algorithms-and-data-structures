a6c38174dae5b3a1ecf0a5d2c4519d934b60e18e
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-10 14:18:24 +0100

    feat: add solution for the "Can Place Flowers" problem

---
+++ neetcode/P0605_CanPlaceFlowers/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace neetcode.P0605_CanPlaceFlowers;
+
+// https://leetcode.com/problems/can-place-flowers/description/
+public class Solution
+{
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        int empty = flowerbed[0] == 0 ? 1 : 0;
+
+        foreach (int f in flowerbed)
+        {
+            if (f == 1)
+            {
+                n -= (empty - 1) / 2;
+                empty = 0;
+            }
+            else
+            {
+                empty++;
+            }
+        }
+
+        n -= empty / 2;
+        return n <= 0;
+    }
+}
