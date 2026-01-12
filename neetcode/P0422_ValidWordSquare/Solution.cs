fec53b9246fdb3cafb0a475f00ede3bc65007b22
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-12 20:22:47 +0100

    feat: add solution for "Valid Word Square" problem

---
+++ neetcode/P0422_ValidWordSquare/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace neetcode.P0422_ValidWordSquare;
+
+// https://leetcode.com/problems/valid-word-square
+// https://neetcode.io/problems/valid-word-square/question
+public class Solution
+{
+    public bool ValidWordSquare(IList<string> words)
+    {
+        for (int wordNum = 0; wordNum < words.Count; wordNum++)
+        {
+            for (int charPos = 0; charPos < words[wordNum].Length; charPos++)
+            {
+                if (
+                    charPos >= words.Count
+                    || wordNum >= words[charPos].Length
+                    || words[wordNum][charPos] != words[charPos][wordNum]
+                )
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
