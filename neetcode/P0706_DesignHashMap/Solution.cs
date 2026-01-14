b9526e048cc21bb0fb35504a609e782a53bbf996
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-14 13:01:25 +0100

    feat: implement MyHashMap class for key-value storage and retrieval

---
+++ neetcode/P0706_DesignHashMap/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace neetcode.P0706_DesignHashMap;
+
+// https://leetcode.com/problems/design-hashmap/description/
+// https://neetcode.io/problems/design-hashmap/question
+public class MyHashMap
+{
+    private int[] map;
+
+    public MyHashMap()
+    {
+        map = new int[1000001];
+        for (int i = 0; i < map.Length; i++)
+        {
+            map[i] = -1;
+        }
+    }
+
+    public void Put(int key, int value)
+    {
+        map[key] = value;
+    }
+
+    public int Get(int key)
+    {
+        return map[key];
+    }
+
+    public void Remove(int key)
+    {
+        map[key] = -1;
+    }
+}
