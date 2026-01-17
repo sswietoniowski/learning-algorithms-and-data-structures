20ab40167e6bad6001b99a0c056e94e824f3a6c1
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-17 12:48:29 +0100

    feat: add solution for "Maximum Ascending Subarray Sum" problem

---
+++ neetcode/P1800_MaximumAscendingSubarraySum/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace neetcode.P1800_MaximumAscendingSubarraySum;
+
+// https://leetcode.com/problems/maximum-ascending-subarray-sum/description/
+public class Solution
+{
+    public int MaxAscendingSum(int[] nums)
+    {
+        int maximumSum = nums[0];
+        int currentSum = nums[0];
+        int previousValue = nums[0];
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > previousValue)
+            {
+                currentSum += nums[i];
+                maximumSum = Math.Max(maximumSum, currentSum);
+            }
+            else
+            {
+                currentSum = nums[i];
+            }
+            previousValue = nums[i];
+        }
+        maximumSum = Math.Max(maximumSum, currentSum);
+
+        return maximumSum;
+    }
+}
