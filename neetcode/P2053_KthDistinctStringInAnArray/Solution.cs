24dee54bf22ca46f760b971b0d11f82e02373742
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-19 09:14:23 +0100

    feat: implement solution for "Kth Distinct String in an Array" problem

---
+++ neetcode/P2053_KthDistinctStringInAnArray/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace neetcode.P2053_KthDistinctStringInAnArray;
+
+// https://leetcode.com/problems/kth-distinct-string-in-an-array/description/
+public class Solution
+{
+    public string KthDistinct(string[] arr, int k)
+    {
+        var counts = new Dictionary<string, int>();
+
+        foreach (var s in arr)
+        {
+            counts[s] = counts.GetValueOrDefault(s) + 1;
+        }
+
+        int distinctCount = 0;
+        foreach (var s in arr)
+        {
+            if (counts[s] == 1)
+            {
+                distinctCount++;
+                if (distinctCount == k)
+                {
+                    return s;
+                }
+            }
+        }
+
+        return "";
+    }
+}
