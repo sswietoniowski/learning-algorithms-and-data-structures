0b7e05c2ddaec4aee57677c67463b97978e632f9
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-20 18:16:29 +0100

    feat: implement Merge Sort algorithm in SortArray method

--- neetcode/P0912_SortAnArray/Solution.cs
+++ neetcode/P0912_SortAnArray/Solution.cs
@@ -6,6 +6,60 @@ public class Solution
 {
     public int[] SortArray(int[] nums)
     {
-        throw new NotImplementedException();
+        if (nums == null || nums.Length <= 1)
+            return nums;
+
+        int[] auxiliary = new int[nums.Length];
+        MergeSort(nums, auxiliary, 0, nums.Length - 1);
+        return nums;
+    }
+
+    private void MergeSort(int[] array, int[] aux, int left, int right)
+    {
+        if (left < right)
+        {
+            int mid = left + (right - left) / 2;
+
+            // divide
+            MergeSort(array, aux, left, mid);
+            MergeSort(array, aux, mid + 1, right);
+
+            // conquer
+            Merge(array, aux, left, mid, right);
+        }
+    }
+
+    private void Merge(int[] array, int[] aux, int left, int mid, int right)
+    {
+        for (int m = left; m <= right; m++)
+        {
+            aux[m] = array[m];
+        }
+
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        while (i <= mid && j <= right)
+        {
+            if (aux[i] <= aux[j])
+            {
+                array[k] = aux[i];
+                i++;
+            }
+            else
+            {
+                array[k] = aux[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i <= mid)
+        {
+            array[k] = aux[i];
+            i++;
+            k++;
+        }
     }
 }
