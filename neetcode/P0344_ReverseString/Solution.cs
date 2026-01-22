65a06d7190c748d7fba7949d6bf79fc4caecb743
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-22 15:11:37 +0100

    feat: add implementation for Reverse String algorithm

---
+++ neetcode/P0344_ReverseString/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace neetcode.P0344_ReverseString;
+
+// https://leetcode.com/problems/reverse-string/description/
+// https://neetcode.io/problems/reverse-string/question?list=neetcode250
+public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        int l = 0,
+            r = s.Length - 1;
+
+        while (l < r)
+        {
+            char tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+}
