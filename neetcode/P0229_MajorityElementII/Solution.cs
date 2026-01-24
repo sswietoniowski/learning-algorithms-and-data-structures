a80db52298277325405256312dc2e0b4ac68b934
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-24 12:57:53 +0100

    feat: implement optimized solution for "Majority Element II" algorithm

--- neetcode/P0229_MajorityElementII/Solution.cs
+++ neetcode/P0229_MajorityElementII/Solution.cs
@@ -3,6 +3,7 @@
 // https://leetcode.com/problems/majority-element-ii/
 // https://neetcode.io/problems/majority-element-ii/question
 // v1
+/*
 public class Solution
 {
     public List<int> MajorityElement(int[] nums)
@@ -31,5 +32,62 @@ public class Solution
             }
         }
         return majorityElements;
+    }
+}
+*/
+// v2
+public class Solution
+{
+    public List<int> MajorityElement(int[] nums)
+    {
+        int n = nums.Length;
+        int num1 = -1,
+            num2 = -1;
+        int cnt1 = 0,
+            cnt2 = 0;
+
+        foreach (int num in nums)
+        {
+            if (num == num1)
+            {
+                cnt1++;
+            }
+            else if (num == num2)
+            {
+                cnt2++;
+            }
+            else if (cnt1 == 0)
+            {
+                num1 = num;
+                cnt1 = 1;
+            }
+            else if (cnt2 == 0)
+            {
+                num2 = num;
+                cnt2 = 1;
+            }
+            else
+            {
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        cnt1 = cnt2 = 0;
+        foreach (int num in nums)
+        {
+            if (num == num1)
+                cnt1++;
+            else if (num == num2)
+                cnt2++;
+        }
+
+        List<int> res = new List<int>();
+        if (cnt1 > n / 3)
+            res.Add(num1);
+        if (cnt2 > n / 3)
+            res.Add(num2);
+
+        return res;
     }
 }
