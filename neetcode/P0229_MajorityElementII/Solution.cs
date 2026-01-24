47fcd7b30c3701e1d09f97204b575914c45adeec
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-24 11:01:20 +0100

    feat: implement "Majority Element II" algorithm

---
+++ neetcode/P0229_MajorityElementII/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace neetcode.P0229_MajorityElementII;
+
+// https://leetcode.com/problems/majority-element-ii/
+// https://neetcode.io/problems/majority-element-ii/question
+// v1
+public class Solution
+{
+    public List<int> MajorityElement(int[] nums)
+    {
+        var numberFrequencies = new Dictionary<int, int>();
+        var uniqueNumbers = new HashSet<int>();
+        foreach (var number in nums)
+        {
+            if (!uniqueNumbers.Contains(number))
+            {
+                numberFrequencies.Add(number, 1);
+                uniqueNumbers.Add(number);
+            }
+            else
+            {
+                numberFrequencies[number]++;
+            }
+        }
+        var cutOffFrequency = Math.Floor(nums.Length / 3.0);
+        var majorityElements = new List<int>();
+        foreach (var (number, frequency) in numberFrequencies)
+        {
+            if (frequency > cutOffFrequency)
+            {
+                majorityElements.Add(number);
+            }
+        }
+        return majorityElements;
+    }
+}
