115cab2da095702ca94363d3b81a0ce3c308082c
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-28 10:11:50 +0100

    feat: add initial implementation for ContainsDuplicate II and Valid Palindrome II algorithms

---
+++ neetcode/P0219_ContainsDuplicateII/Solution.cs
@@ -0,0 +1,8 @@
+﻿namespace neetcode.P0219_ContainsDuplicateII;
+
+// https://leetcode.com/problems/contains-duplicate-ii/
+// https://neetcode.io/problems/contains-duplicate-ii/question?list=neetcode250
+public class Solution
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k) { }
+}
---
+++ neetcode/P0680_ValidPalindromeII/Solution.cs
@@ -0,0 +1,8 @@
+﻿namespace neetcode.P0680_ValidPalindromeII;
+
+// https://leetcode.com/problems/valid-palindrome-ii/description/
+// https://neetcode.io/problems/valid-palindrome-ii/question?list=neetcode250
+public class Solution
+{
+    public bool ValidPalindrome(string s) { }
+}
