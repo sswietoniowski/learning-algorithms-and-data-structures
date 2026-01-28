3a08b0852cfcf5dc10929002933d1d58eccb7fa0
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-28 14:52:26 +0100

    feat: add implementation for Minimum Size Subarray Sum algorithm

---
+++ neetcode/P0209_MinimumSizeSubarraySum/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace neetcode.P0209_MinimumSizeSubarraySum;
+
+// https://leetcode.com/problems/minimum-size-subarray-sum/description/
+// https://neetcode.io/problems/minimum-size-subarray-sum/question
+public class Solution
+{
+    public int MinSubArrayLen(int target, int[] nums)
+    {
+        int l = 0;
+        int total = 0;
+        int result = nums.Length + 1;
+
+        for (int r = 0; r < nums.Length; r++)
+        {
+            total += nums[r];
+            while (total >= target)
+            {
+                result = Math.Min(result, r - l + 1);
+                total -= nums[l];
+                l++;
+            }
+        }
+
+        return result == nums.Length + 1 ? 0 : result;
+    }
+}
