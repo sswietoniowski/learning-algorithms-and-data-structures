741347d3e612f4e7860f376871b918a2054e759a
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-28 10:43:49 +0100

    feat: implement ContainsNearbyDuplicate algorithm

--- neetcode/P0219_ContainsDuplicateII/Solution.cs
+++ neetcode/P0219_ContainsDuplicateII/Solution.cs
@@ -2,7 +2,28 @@
 
 // https://leetcode.com/problems/contains-duplicate-ii/
 // https://neetcode.io/problems/contains-duplicate-ii/question?list=neetcode250
+// v1
 public class Solution
 {
-    public bool ContainsNearbyDuplicate(int[] nums, int k) { }
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        HashSet<int> window = new HashSet<int>();
+        int L = 0;
+
+        for (int R = 0; R < nums.Length; R++)
+        {
+            if (R - L > k)
+            {
+                window.Remove(nums[L]);
+                L++;
+            }
+            if (window.Contains(nums[R]))
+            {
+                return true;
+            }
+            window.Add(nums[R]);
+        }
+
+        return false;
+    }
 }
