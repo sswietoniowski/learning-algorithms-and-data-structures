e07392b23a5f2e95cfee5dcc7b9e4d8184b34fbc
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-29 17:54:38 +0100

    refactor: simplify Merge function implementation for merging two sorted arrays

--- neetcode/P0088_MergeSortedArray/Solution.cs
+++ neetcode/P0088_MergeSortedArray/Solution.cs
@@ -2,6 +2,8 @@ namespace neetcode.P0088_MergeSortedArray;
 
 // https://leetcode.com/problems/merge-sorted-array/description/
 // https://neetcode.io/problems/merge-sorted-array/question?list=neetcode250
+// v1
+/*
 public class Solution
 {
     public void Merge(int[] nums1, int m, int[] nums2, int n)
@@ -23,6 +25,29 @@ public class Solution
                 i1--;
             }
             i3--;
+        }
+    }
+}
+*/
+// v2
+public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int i1 = m - 1;
+        int i2 = n - 1;
+        int i3 = m + n - 1;
+
+        while (i2 >= 0)
+        {
+            if (i1 >= 0 && nums1[i1] > nums2[i2])
+            {
+                nums1[i3--] = nums1[i1--];
+            }
+            else
+            {
+                nums1[i3--] = nums2[i2--];
+            }
         }
     }
 }
