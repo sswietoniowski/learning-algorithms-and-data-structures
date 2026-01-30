e5b96867a3041800b4b149ef9fda68e9ebb5266d
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-30 14:24:29 +0100

    feat: implement MinimumRecolors function to calculate minimum recolors for k consecutive black blocks

--- neetcode/P2379_MinimumRecolors/Solution.cs
+++ neetcode/P2379_MinimumRecolors/Solution.cs
@@ -4,5 +4,31 @@
 // https://neetcode.io/problems/minimum-recolors-to-get-k-consecutive-black-blocks/question?list=allNC
 public class Solution
 {
-    public int MinimumRecolors(string blocks, int k) { }
+    public int MinimumRecolors(string blocks, int k)
+    {
+        int count_w = 0;
+        for (int i = 0; i < k; i++)
+        {
+            if (blocks[i] == 'W')
+            {
+                count_w++;
+            }
+        }
+
+        int res = count_w;
+        for (int i = k; i < blocks.Length; i++)
+        {
+            if (blocks[i - k] == 'W')
+            {
+                count_w--;
+            }
+            if (blocks[i] == 'W')
+            {
+                count_w++;
+            }
+            res = Math.Min(res, count_w);
+        }
+
+        return res;
+    }
 }
