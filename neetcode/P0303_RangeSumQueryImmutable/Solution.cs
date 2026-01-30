01cfda7b5ea1f4368431cbaec2af7bd3119516b6
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-30 13:03:36 +0100

    feat: implement optimized NumArray class with prefix sum array for efficient range sum queries

--- neetcode/P0303_RangeSumQueryImmutable/Solution.cs
+++ neetcode/P0303_RangeSumQueryImmutable/Solution.cs
@@ -2,13 +2,46 @@
 
 // https://leetcode.com/problems/range-sum-query-immutable/description/
 // https://neetcode.io/problems/range-sum-query-immutable/question?list=allNC
+// v1
+/*
+public class NumArray {
+    private int[] nums;
+
+    public NumArray(int[] nums) {
+        this.nums = nums;
+    }
+    
+    public int SumRange(int left, int right) {
+        int sum = 0;
+
+        for int (i = left; i <= right; i++)
+        {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+}
+*/
+// v2
 public class NumArray
 {
-    public NumArray(int[] nums) { }
+    private int[] prefixes;
 
-    public int SumRange(int left, int right) { }
+    public NumArray(int[] nums)
+    {
+        prefixes = new int[nums.Length + 1];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            prefixes[i + 1] = prefixes[i] + nums[i];
+        }
+    }
+
+    public int SumRange(int left, int right)
+    {
+        return prefixes[right + 1] - prefixes[left];
+    }
 }
-
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray obj = new NumArray(nums);
