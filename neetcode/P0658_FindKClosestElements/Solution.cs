9c5aa7f27859a086e581847c39922a4bf34978ca
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-30 09:25:12 +0100

    feat: implement FindClosestElements function to retrieve k closest elements to x

--- neetcode/P0658_FindKClosestElements/Solution.cs
+++ neetcode/P0658_FindKClosestElements/Solution.cs
@@ -4,5 +4,27 @@
 // https://neetcode.io/problems/find-k-closest-elements/question?list=neetcode250
 public class Solution
 {
-    public IList<int> FindClosestElements(int[] arr, int k, int x) { }
+    public IList<int> FindClosestElements(int[] arr, int k, int x)
+    {
+        int l = 0,
+            r = arr.Length - k;
+        while (l < r)
+        {
+            int m = (l + r) / 2;
+            if (x - arr[m] > arr[m + k] - x)
+            {
+                l = m + 1;
+            }
+            else
+            {
+                r = m;
+            }
+        }
+        var result = new List<int>();
+        for (int i = l; i < l + k; i++)
+        {
+            result.Add(arr[i]);
+        }
+        return result;
+    }
 }
