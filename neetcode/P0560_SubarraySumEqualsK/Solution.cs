6a56d9c810d09970dc2289eb68299e9c1c2e3853
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-30 09:24:51 +0100

    feat: implement SubarraySum function to count subarrays that sum to k

--- neetcode/P0560_SubarraySumEqualsK/Solution.cs
+++ neetcode/P0560_SubarraySumEqualsK/Solution.cs
@@ -4,5 +4,30 @@
 // https://neetcode.io/problems/subarray-sum-equals-k/question?list=neetcode250
 public class Solution
 {
-    public int SubarraySum(int[] nums, int k) { }
+    public int SubarraySum(int[] nums, int k)
+    {
+        int res = 0,
+            curSum = 0;
+        Dictionary<int, int> prefixSums = new Dictionary<int, int>();
+        prefixSums[0] = 1;
+
+        foreach (int num in nums)
+        {
+            curSum += num;
+            int diff = curSum - k;
+
+            if (prefixSums.ContainsKey(diff))
+            {
+                res += prefixSums[diff];
+            }
+
+            if (!prefixSums.ContainsKey(curSum))
+            {
+                prefixSums[curSum] = 0;
+            }
+            prefixSums[curSum]++;
+        }
+
+        return res;
+    }
 }
