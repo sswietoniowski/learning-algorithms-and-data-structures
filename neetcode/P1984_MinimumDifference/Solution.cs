a09fffa089b1943c9c2e56a9b11ccb40c45a571c
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-01-31 18:33:10 +0100

    feat: implement MinimumDifference function to calculate minimum difference between highest and lowest of k scores

---
+++ neetcode/P1984_MinimumDifference/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace neetcode.P1984_MinimumDifference;
+
+// https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/description/
+// https://neetcode.io/problems/minimum-difference-between-highest-and-lowest-of-k-scores/question
+public class Solution
+{
+    public int MinimumDifference(int[] nums, int k)
+    {
+        if (k == 1)
+        {
+            return 0;
+        }
+
+        Array.Sort(nums);
+
+        int minDifference = int.MaxValue;
+
+        for (int i = k - 1; i < nums.Length; i++)
+        {
+            int currentDifference = nums[i] - nums[i - k + 1];
+            if (currentDifference < minDifference)
+            {
+                minDifference = currentDifference;
+            }
+        }
+
+        return minDifference;
+    }
+}
