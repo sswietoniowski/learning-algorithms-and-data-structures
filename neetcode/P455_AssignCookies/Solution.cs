6cb374ed3a7bb47029f214ab6254d43b72877701
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-01 16:46:41 +0100

    feat: implement FindContentChildren function to determine the maximum number of children that can be content with given cookies

---
+++ neetcode/P455_AssignCookies/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace neetcode.P455_AssignCookies;
+
+// https://leetcode.com/problems/assign-cookies/description/
+// https://neetcode.io/problems/assign-cookies/question
+public class Solution
+{
+    public int FindContentChildren(int[] g, int[] s)
+    {
+        Array.Sort(g);
+        Array.Sort(s);
+
+        int childIndex = 0;
+        int cookieIndex = 0;
+
+        while (childIndex < g.Length && cookieIndex < s.Length)
+        {
+            if (s[cookieIndex] >= g[childIndex])
+            {
+                childIndex++;
+            }
+
+            cookieIndex++;
+        }
+
+        return childIndex;
+    }
+}
