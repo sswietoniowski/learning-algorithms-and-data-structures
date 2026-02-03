db59f320ac8aee485ae45abb5a102b6ae6f52b54
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-03 21:13:39 +0100

    feat: implement SearchInsert function to determine the index to insert a target in a sorted array

---
+++ neetcode/P0035_SearchInsertPosition/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace neetcode.P0035_SearchInsertPosition;
+
+// https://leetcode.com/problems/search-insert-position/description/
+// https://neetcode.io/problems/search-insert-position/question
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
