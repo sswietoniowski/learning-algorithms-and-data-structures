e658e91c612b0b7dfd3a82fbb7df4fea12b03388
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-03 20:55:54 +0100

    feat: implement MinOperations function to calculate the minimum number of operations required to navigate a folder structure

---
+++ neetcode/P1598_CrawlerLogFolder/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace neetcode.P1598_CrawlerLogFolder;
+
+// https://leetcode.com/problems/crawler-log-folder/description/
+// https://neetcode.io/problems/crawler-log-folder/question
+public class Solution
+{
+    public int MinOperations(string[] logs)
+    {
+        var levels = new Stack<string>();
+
+        foreach (var log in logs)
+        {
+            switch (log)
+            {
+                case "../":
+                    if (levels.Count > 0)
+                    {
+                        levels.Pop();
+                    }
+                    break;
+                case "./":
+                    break;
+                default:
+                    levels.Push(log);
+                    break;
+            }
+        }
+
+        return levels.Count;
+    }
+}
