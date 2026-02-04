c44204864b3b265c2ed30861a084a2a988e93e97
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-04 12:34:08 +0100

    feat: add implementation of IsPerfectSquare method to determine if a number is a perfect square

---
+++ neetcode/P0367_ValidPerfectSquare/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace neetcode.P0367_ValidPerfectSquare;
+
+// https://leetcode.com/problems/valid-perfect-square/description/
+// https://neetcode.io/problems/valid-perfect-square/question
+// v1
+public class Solution
+{
+    public bool IsPerfectSquare(int num)
+    {
+        if (num < 2)
+            return true;
+
+        int left = 1;
+        int right = num / 2;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            long result = (long)mid * mid;
+            if (result == num)
+            {
+                return true;
+            }
+            else if (result < num)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
