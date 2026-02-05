3a1ef28ea5f7608688b3d8cfd21e4c4c7e1dc532
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-05 12:19:49 +0100

    feat: add RecoverOrder method to restore finishing order based on friends list

---
+++ leetcode/P3668_RestoreFinishingOrder/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode.P3668_RestoreFinishingOrder;
+
+// https://leetcode.com/problems/restore-finishing-order/description/
+public class Solution
+{
+    public int[] RecoverOrder(int[] order, int[] friends)
+    {
+        var result = new int[friends.Length];
+        var set = new HashSet<int>();
+        foreach (var f in friends)
+        {
+            set.Add(f);
+        }
+        int i = 0;
+        foreach (var o in order)
+        {
+            if (set.Contains(o))
+            {
+                result[i] = o;
+                i++;
+            }
+        }
+        return result;
+    }
+}
