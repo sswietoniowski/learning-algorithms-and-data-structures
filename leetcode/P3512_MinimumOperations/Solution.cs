0297b0f8eb90ed8d59f385d033252764499efbc2
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-05 12:29:38 +0100

    feat: add initial implementation of MinOperations method to calculate array sum divisibility

---
+++ leetcode/P3512_MinimumOperations/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace leetcode.P3512_MinimumOperations;
+
+// https://leetcode.com/problems/minimum-operations-to-make-array-sum-divisible-by-k/description/
+// v1
+public class Solution
+{
+    public int MinOperations(int[] nums, int k)
+    {
+        int sum = 0;
+        foreach (var n in nums)
+        {
+            sum += n;
+        }
+        return sum % k;
+    }
+}
