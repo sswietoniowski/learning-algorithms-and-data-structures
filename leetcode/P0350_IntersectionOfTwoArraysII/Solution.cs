648ca5256de7f886dd472b1ad02c946942f5d2ad
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-05 13:36:55 +0100

    feat: add initial implementation of Intersect method to find intersection of two arrays

---
+++ leetcode/P0350_IntersectionOfTwoArraysII/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode.P0350_IntersectionOfTwoArraysII;
+
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+// v1
+public class Solution
+{
+    public int[] Intersect(int[] nums1, int[] nums2)
+    {
+        int[] counts1 = new int[1001];
+        foreach (var n in nums1)
+        {
+            counts1[n]++;
+        }
+        int[] counts2 = new int[1001];
+        foreach (var n in nums2)
+        {
+            counts2[n]++;
+        }
+        var result = new List<int>();
+        for (int i = 0; i < counts1.Length; i++)
+        {
+            if (counts1[i] > 0 && counts2[i] > 0)
+            {
+                for (int j = 0; j < Math.Min(counts1[i], counts2[i]); j++)
+                {
+                    result.Add(i);
+                }
+            }
+        }
+        return result.ToArray();
+    }
+}
