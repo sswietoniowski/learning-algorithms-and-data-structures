99e44e15cc93b3af9887a317987b4387477ecd7e
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-05 09:47:04 +0100

    feat: implement optimized ConstructTransformedArray method for improved performance

---
+++ leetcode/P3379_TransformedArray/Solution.cs
@@ -0,0 +1,58 @@
+﻿namespace leetcode.P3379_TransformedArray;
+
+// https://leetcode.com/problems/transformed-array/description/?envType=daily-question&envId=2026-02-05
+// v1
+/*
+public class Solution
+{
+    public int[] ConstructTransformedArray(int[] nums)
+    {
+        var result = new int[nums.Length];
+
+        for (int i = 0, j = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > 0)
+            {
+                j = (i + nums[i]) % nums.Length;
+            }
+            else if (nums[i] < 0)
+            {
+                if (i - Math.Abs(nums[i]) > 0)
+                {
+                    j = i - Math.Abs(nums[i]);
+                }
+                else
+                {
+                    j =
+                        (nums.Length - Math.Abs((i - Math.Abs(nums[i]))) % nums.Length)
+                        % nums.Length;
+                }
+            }
+            else
+            {
+                j = i;
+            }
+            result[i] = nums[j];
+        }
+
+        return result;
+    }
+}
+*/
+// v2
+public class Solution
+{
+    public int[] ConstructTransformedArray(int[] nums)
+    {
+        int n = nums.Length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            int targetIndex = ((i + nums[i]) % n + n) % n;
+            result[i] = nums[targetIndex];
+        }
+
+        return result;
+    }
+}
