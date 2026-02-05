74afaf574f2fc0113c27b40f4fd2bf089500bcce
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-05 21:35:51 +0100

    feat: add ArrangeCoins method to calculate the number of complete rows of coins that can be arranged

---
+++ neetcode/P0441_ArrangingCoins/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace neetcode.P0441_ArrangingCoins;
+
+// https://leetcode.com/problems/arranging-coins/description/
+// https://neetcode.io/problems/arranging-coins/question
+public class Solution
+{
+    public int ArrangeCoins(int n)
+    {
+        int left = 1;
+        int right = n;
+        int rows = 0;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (((long)mid * mid + mid) / 2 > (long)n)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                rows = mid;
+                left = mid + 1;
+            }
+        }
+        return rows;
+    }
+}
