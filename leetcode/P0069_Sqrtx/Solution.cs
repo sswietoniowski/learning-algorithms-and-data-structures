c6e546d9b59e6b72c0cb62e73a2b59a196c4bf86
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-05 12:49:10 +0100

    feat: add MySqrt method for calculating the square root using binary search

---
+++ leetcode/P0069_Sqrtx/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode.P0069_Sqrtx;
+
+// https://leetcode.com/problems/sqrtx/description/
+public class Solution
+{
+    public int MySqrt(int x)
+    {
+        if (x < 2)
+            return x;
+
+        int left = 1;
+        int right = x / 2;
+        int result = 0;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            long square = (long)mid * mid;
+
+            if (square == x)
+            {
+                return mid;
+            }
+            else if (square < x)
+            {
+                result = mid;
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
