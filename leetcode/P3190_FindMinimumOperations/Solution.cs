6b297e525b31e6066001d221d58700317f4ec451
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-05 11:25:07 +0100

    feat: implement MinimumOperations method to count operations for divisibility by three

---
+++ leetcode/P3190_FindMinimumOperations/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode.P3190_FindMinimumOperations;
+
+// https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/description/
+public class Solution
+{
+    public int MinimumOperations(int[] nums)
+    {
+        int operations = 0;
+        foreach (var n in nums)
+        {
+            if (n % 3 != 0)
+            {
+                operations++;
+            }
+        }
+        return operations;
+    }
+}
