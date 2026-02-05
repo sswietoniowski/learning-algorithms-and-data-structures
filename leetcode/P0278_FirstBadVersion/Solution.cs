f6ccd9ae548559a69a455d36c555b58d1093eab8
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-05 12:56:17 +0100

    feat: implement FirstBadVersion method to identify the first bad version using binary search

---
+++ leetcode/P0278_FirstBadVersion/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode.P0278_FirstBadVersion;
+
+// https://leetcode.com/problems/first-bad-version/description/
+/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+public class VersionControl
+{
+    protected bool IsBadVersion(int version)
+    {
+        // This is a placeholder for the actual implementation.
+        return false;
+    }
+}
+
+public class Solution : VersionControl
+{
+    public int FirstBadVersion(int n)
+    {
+        int left = 1;
+        int right = n;
+        int result = 1;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (IsBadVersion(mid))
+            {
+                result = mid;
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+}
