7d31988ddac1af4bda8b5531fb88c15a4bc25df8
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-05 10:01:06 +0100

    feat: add initial implementation of BuildArray method for array transformation

---
+++ leetcode/P1920_BuildArrayFromPermutation/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace leetcode.P1920_BuildArrayFromPermutation;
+
+// https://leetcode.com/problems/build-array-from-permutation/description/
+public class Solution
+{
+    public int[] BuildArray(int[] nums)
+    {
+        var result = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            result[i] = nums[nums[i]];
+        }
+        return result;
+    }
+}
