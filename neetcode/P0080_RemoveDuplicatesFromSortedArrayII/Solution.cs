00d907a8db7ba2007de97363c83c96df832e1ff3
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-06 13:27:28 +0100

    feat: add RemoveDuplicates method to remove duplicates from sorted array II

---
+++ neetcode/P0080_RemoveDuplicatesFromSortedArrayII/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace neetcode.P0080_RemoveDuplicatesFromSortedArrayII;
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int n = nums.Length;
+        if (n <= 2)
+        {
+            return n;
+        }
+
+        int left = 2;
+        for (int right = 2; right < n; right++)
+        {
+            if (nums[right] != nums[left - 2])
+            {
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+
+        return left;
+    }
+}
