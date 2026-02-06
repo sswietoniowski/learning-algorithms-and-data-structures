8a884e169379f8da16a43fc1c91ef2dc1ece859e
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-06 17:37:36 +0100

    feat: add SingleNonDuplicate method to find the single non-duplicate element in a sorted array

---
+++ neetcode/P0540_SingleElementInASortedArray/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace neetcode.P0540_SingleElementInASortedArray;
+
+// https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+// https://neetcode.io/problems/single-element-in-a-sorted-array/question
+public class Solution
+{
+    public int SingleNonDuplicate(int[] nums)
+    {
+        int start = 0;
+        int end = nums.Length - 1;
+        while (start < end)
+        {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == nums[mid ^ 1])
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid;
+            }
+        }
+        return nums[start];
+    }
+}
