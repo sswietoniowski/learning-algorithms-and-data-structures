36d8e0d67f1088e0407ac76c45092635e53f6a71
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-06 17:06:23 +0100

    feat: implement AsteroidCollision method to handle asteroid collisions

---
+++ neetcode/P0735_AsteroidCollision/Solution.cs
@@ -0,0 +1,57 @@
+﻿namespace neetcode.P0735_AsteroidCollision;
+
+// https://leetcode.com/problems/asteroid-collision/description/
+// https://neetcode.io/problems/asteroid-collision/question?list=allNC
+public class Solution
+{
+    public int[] AsteroidCollision(int[] asteroids)
+    {
+        var stack = new Stack<int>();
+        foreach (var asteroid in asteroids)
+        {
+            if (asteroid > 0)
+            {
+                stack.Push(asteroid);
+            }
+            else
+            {
+                if (stack.Count == 0 || stack.Peek() < 0)
+                {
+                    stack.Push(asteroid);
+                }
+                else
+                {
+                    bool destroyed = false;
+                    while (stack.Count > 0 && stack.Peek() > 0)
+                    {
+                        if (stack.Peek() < Math.Abs(asteroid))
+                        {
+                            stack.Pop();
+                            continue;
+                        }
+                        else if (stack.Peek() == Math.Abs(asteroid))
+                        {
+                            stack.Pop();
+                            destroyed = true;
+                            break;
+                        }
+                        else
+                        {
+                            destroyed = true;
+                            break;
+                        }
+                    }
+
+                    if (!destroyed)
+                    {
+                        stack.Push(asteroid);
+                    }
+                }
+            }
+        }
+
+        int[] result = stack.ToArray();
+        Array.Reverse(result);
+        return result;
+    }
+}
