164927b5d055d4de2c51b5d7f142826e47ca5e18
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-06 09:13:38 +0100

    feat: add MinRemoval method to calculate minimum removals to balance array

---
+++ leetcode/P3634_MinimumRemovalsToBalanceArray/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace leetcode.P3634_MinimumRemovalsToBalanceArray;
+
+// https://leetcode.com/problems/minimum-removals-to-balance-array/description/?envType=daily-question&envId=2026-02-06
+// v1
+public class Solution
+{
+    public int MinRemoval(int[] nums, int k)
+    {
+        int n = nums.Length;
+        if (n <= 1)
+            return 0;
+
+        Array.Sort(nums);
+
+        int maxKept = 0;
+        int l = 0;
+
+        for (int r = 0; r < n; r++)
+        {
+            while ((long)nums[r] > (long)nums[l] * k)
+            {
+                l++;
+            }
+
+            int currentWindowSize = r - l + 1;
+            if (currentWindowSize > maxKept)
+            {
+                maxKept = currentWindowSize;
+            }
+        }
+
+        return n - maxKept;
+    }
+}
