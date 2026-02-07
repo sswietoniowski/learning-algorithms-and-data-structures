e571ee051c3f8cc2b5c4bb1123991792ab39054d
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-07 16:08:32 +0100

    feat: implement FindPeakElement method to locate a peak element in an array

---
+++ leetcode/P0162_FindPeakElement/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode.P0162_FindPeakElement;
+
+// https://leetcode.com/problems/find-peak-element/description/
+public class Solution
+{
+    public int FindPeakElement(int[] nums)
+    {
+        int start = 0;
+        int end = nums.Length - 1;
+        while (start < end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] < nums[mid + 1])
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid;
+            }
+        }
+        return start;
+    }
+}
