f4a85d7e1da595b2def82f2c825c06edd9ce9aa0
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-07 12:34:36 +0100

    feat: implement MinimumDeletions method to calculate minimum deletions for balanced string

---
+++ leetcode/P1653_MinimumDeletions/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode.P1653_MinimumDeletions;
+
+// https://leetcode.com/problems/minimum-deletions-to-make-string-balanced/?envType=daily-question&envId=2026-02-07
+public class Solution
+{
+    public int MinimumDeletions(string s)
+    {
+        int deletions = 0;
+        int bCount = 0;
+
+        foreach (char c in s)
+        {
+            if (c == 'b')
+            {
+                bCount++;
+            }
+            else
+            {
+                if (bCount > 0)
+                {
+                    deletions = Math.Min(deletions + 1, bCount);
+                }
+            }
+        }
+
+        return deletions;
+    }
+}
