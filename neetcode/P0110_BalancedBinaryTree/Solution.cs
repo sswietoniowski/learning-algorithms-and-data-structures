955c3f495c5f74a3cab2adc90c357afff26e534c
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-08 11:40:35 +0100

    refactor: optimize dfs method in Solution class for balanced binary tree

--- neetcode/P0110_BalancedBinaryTree/Solution.cs
+++ neetcode/P0110_BalancedBinaryTree/Solution.cs
@@ -5,6 +5,7 @@ public class TreeNode
     public int val;
     public TreeNode left;
     public TreeNode right;
+
     public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
     {
         this.val = val;
@@ -29,6 +30,8 @@ public class TreeNode
 
 // https://leetcode.com/problems/balanced-binary-tree/
 // https://youtu.be/QfJsau0ItOY
+// v1
+/*
 public class Solution
 {
     private (bool, int) dfs(TreeNode root)
@@ -46,6 +49,39 @@ public class Solution
         return (balanced, Math.Max(leftHeight, rightHeight) + 1);
     }
     
+    public bool IsBalanced(TreeNode root)
+    {
+        var (balanced, _) = dfs(root);
+        return balanced;
+    }
+}
+*/
+// v2
+public class Solution
+{
+    private (bool, int) dfs(TreeNode root)
+    {
+        if (root is null)
+        {
+            return (true, 0);
+        }
+
+        var (leftBalanced, leftHeight) = dfs(root.left);
+        // Optimization: Stop immediately if left is bad
+        if (!leftBalanced)
+            return (false, 0);
+
+        var (rightBalanced, rightHeight) = dfs(root.right);
+        // Optimization: Stop immediately if right is bad
+        if (!rightBalanced)
+            return (false, 0);
+
+        // Now we only need to check the height difference
+        bool balanced = Math.Abs(leftHeight - rightHeight) <= 1;
+
+        return (balanced, Math.Max(leftHeight, rightHeight) + 1);
+    }
+
     public bool IsBalanced(TreeNode root)
     {
         var (balanced, _) = dfs(root);
