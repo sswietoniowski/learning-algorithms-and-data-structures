73b13a0e549a925792faa384eccc5b6767bb591f
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-08 16:09:18 +0100

    feat: implement ShipWithinDays method to calculate minimum capacity to ship packages within D days

---
+++ neetcode/P1011_CapacityToShipPackagesWithinDDays/Solution.cs
@@ -0,0 +1,56 @@
+﻿namespace neetcode.P1011_CapacityToShipPackagesWithinDDays;
+
+// https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/description/
+// https://neetcode.io/problems/capacity-to-ship-packages-within-d-days/question
+public class Solution
+{
+    public int ShipWithinDays(int[] weights, int days)
+    {
+        int maxWeight = 0;
+        int sumOfWeights = 0;
+        foreach (var weight in weights)
+        {
+            maxWeight = Math.Max(weight, maxWeight);
+            sumOfWeights += weight;
+        }
+
+        int start = maxWeight;
+        int end = sumOfWeights;
+        while (start < end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (IsEnoughCapacity(weights, days, mid))
+            {
+                end = mid;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
+
+    private bool IsEnoughCapacity(int[] weights, int days, int capacity)
+    {
+        int totalDays = 1;
+        int currentDayLoad = 0;
+
+        foreach (var weight in weights)
+        {
+            if (currentDayLoad + weight > capacity)
+            {
+                currentDayLoad = weight;
+                totalDays++;
+            }
+            else
+            {
+                currentDayLoad += weight;
+            }
+        }
+
+        return totalDays <= days;
+    }
+}
