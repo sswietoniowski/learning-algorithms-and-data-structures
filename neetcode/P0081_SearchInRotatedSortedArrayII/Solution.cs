321c2e4f6311f3d2a79fca5f3ebe18ed2623718b
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-08 20:26:53 +0100

    feat: implement Search method to find target in rotated sorted array

---
+++ neetcode/P0081_SearchInRotatedSortedArrayII/Solution.cs
@@ -0,0 +1,53 @@
+﻿namespace neetcode.P0081_SearchInRotatedSortedArrayII;
+
+// https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
+// https://neetcode.io/problems/search-in-rotated-sorted-array-ii/question
+public class Solution
+{
+    public bool Search(int[] nums, int target)
+    {
+        int n = nums.Length;
+        int start = 0;
+        int end = n - 1;
+
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target)
+            {
+                return true;
+            }
+
+            if (nums[start] == nums[mid])
+            {
+                start++;
+                continue;
+            }
+            else if (nums[start] < nums[mid])
+            {
+                if (target >= nums[start] && target < nums[mid])
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+            else
+            {
+                if (target > nums[mid] && target <= nums[end])
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
