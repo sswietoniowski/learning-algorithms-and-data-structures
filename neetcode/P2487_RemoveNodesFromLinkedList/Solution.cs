f1fcb840ba85cab1a0e2d18df55a2dce4a954f28
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-10 15:40:40 +0100

    feat: implement RemoveNodes method to remove nodes from a linked list based on value comparison

---
+++ neetcode/P2487_RemoveNodesFromLinkedList/Solution.cs
@@ -0,0 +1,65 @@
+﻿namespace neetcode.P2487_RemoveNodesFromLinkedList;
+
+// https://leetcode.com/problems/remove-nodes-from-linked-list/description/
+// https://neetcode.io/problems/append-characters-to-string-to-make-subsequence/question?list=allNC
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode RemoveNodes(ListNode head)
+    {
+        if (head == null)
+            return null;
+
+        ListNode reversedHead = ReverseNodes(head);
+        ListNode current = reversedHead;
+        while (current.next != null)
+        {
+            if (current.val > current.next.val)
+            {
+                current.next = current.next.next;
+            }
+            else
+            {
+                current = current.next;
+            }
+        }
+        return ReverseNodes(reversedHead);
+    }
+
+    private ListNode ReverseNodes(ListNode head)
+    {
+        ListNode previous = null;
+        ListNode current = head;
+
+        while (current != null)
+        {
+            ListNode tmp = current.next;
+            current.next = previous;
+            previous = current;
+            current = tmp;
+        }
+        return previous;
+    }
+}
