92338ecf2d4a263776d632afbd6a4ba23cf3ed42
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-10 11:17:10 +0100

    feat: implement RemoveElements method to remove elements from a linked list

---
+++ neetcode/P0203_RemoveLinkedListElements/Solution.cs
@@ -0,0 +1,47 @@
+﻿namespace neetcode.P0203_RemoveLinkedListElements;
+
+// https://leetcode.com/problems/remove-linked-list-elements/
+// https://neetcode.io/problems/remove-linked-list-elements/question
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode RemoveElements(ListNode head, int val)
+    {
+        ListNode dummy = new ListNode(0, head);
+        ListNode current = dummy;
+        while (current.next is not null)
+        {
+            if (current.next.val == val)
+            {
+                current.next = current.next.next;
+            }
+            else
+            {
+                current = current.next;
+            }
+        }
+        return dummy.next;
+    }
+}
