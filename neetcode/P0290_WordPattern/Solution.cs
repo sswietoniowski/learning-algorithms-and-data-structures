0022c9ae25c8eef9c11829c60f2a4eeca5eec99c
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-14 10:09:04 +0100

    feat: implement solution for LeetCode problem "290. Word Pattern"

---
+++ neetcode/P0290_WordPattern/Solution.cs
@@ -0,0 +1,45 @@
+﻿namespace neetcode.P0290_WordPattern;
+
+// https://leetcode.com/problems/word-pattern/description/
+// https://neetcode.io/problems/word-pattern/question?list=allNC
+public class Solution
+{
+    public bool WordPattern(string pattern, string s)
+    {
+        string[] words = s.Split(' ');
+
+        if (pattern.Length != words.Length)
+        {
+            return false;
+        }
+
+        Dictionary<char, string> charToWord = new Dictionary<char, string>();
+        Dictionary<string, char> wordToChar = new Dictionary<string, char>();
+
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            char c = pattern[i];
+            string w = words[i];
+
+            if (charToWord.ContainsKey(c))
+            {
+                if (charToWord[c] != w)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (wordToChar.ContainsKey(w))
+                {
+                    return false;
+                }
+
+                charToWord.Add(c, w);
+                wordToChar.Add(w, c);
+            }
+        }
+
+        return true;
+    }
+}
