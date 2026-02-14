88488585b58e0b93142aa8483ca4b8e76cf076db
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-14 12:07:07 +0100

    feat: implement solution for LeetCode problem "P3010. Divide an Array into Subarrays with Minimum Cost"

---
+++ leetcode/P3010_DivideAnArray/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode.P3010_DivideAnArray;
+
+// https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-i/description/?envType=daily-question&envId=2026-02-01
+public class Solution
+{
+    public int MinimumCost(int[] nums)
+    {
+        int sum = nums[0];
+
+        int min1 = int.MaxValue;
+        int min2 = int.MaxValue;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            int current = nums[i];
+
+            if (current < min1)
+            {
+                min2 = min1;
+                min1 = current;
+            }
+            else if (current < min2)
+            {
+                min2 = current;
+            }
+        }
+
+        return sum + min1 + min2;
+    }
+}
