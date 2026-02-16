cfc68f9e10c17e31123410ae0ec6fff521ae4794
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-16 08:43:10 +0100

    feat: simplify ReverseBits implementation and remove unused code

--- neetcode/P0190_ReverseBits/Solution.cs
+++ neetcode/P0190_ReverseBits/Solution.cs
@@ -1,46 +1,19 @@
 ﻿namespace neetcode.P0190_ReverseBits;
 
+// https://leetcode.com/problems/reverse-bits/description/?envType=daily-question&envId=2026-02-16
 public class Solution
 {
-    // https://leetcode.com/problems/reverse-bits/
-    // https://youtu.be/UcoN6UjAI64
-    // v1
-    //public uint reverseBits(uint n)
-    //{
-    //    //uint result = 0;
-
-    //    //uint inMask = 1;
-    //    //uint outMask = (uint)1 << 31;
-    //    //for (int i = 0; i < 32; i++)
-    //    //{
-    //    //    if ((n & inMask) != 0)
-    //    //    {
-    //    //        result |= outMask;
-    //    //    }
-
-    //    //    inMask <<= 1;
-    //    //    outMask >>= 1;
-    //    //}
-
-    //    //return result;
-    //}
-
-    // v2
-    private byte reverseByte(byte b)
+    public int ReverseBits(int n)
     {
-        return (byte)((((uint)b) * 0x0202020202 & 0x010884422010) % 1023);
-    }
-
-    public uint reverseBits(uint n)
-    {
-        uint result = 0;
-
-        for (int i = 0; i < 4; i++)
+        int result = 0;
+        for (int i = 0; i < 32; i++)
         {
-            byte b = (byte)((n >> (i * 8)) & 0xff);
-            result += ((uint)reverseByte(b)) << ((3 - i) * 8);
-        }
+            result = result << 1;
 
+            result = result | (n & 1);
+
+            n = n >> 1;
+        }
         return result;
     }
 }
