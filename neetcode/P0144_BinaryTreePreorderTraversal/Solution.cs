8d31736487e9fc23ff5e4906b85e1c997d58e819
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-16 20:50:11 +0100

    feat: implement Binary Tree Preorder Traversal solution

---
+++ neetcode/P0144_BinaryTreePreorderTraversal/Solution.cs
@@ -0,0 +1,64 @@
+﻿namespace neetcode.P0144_BinaryTreePreorderTraversal;
+
+// https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+// https://neetcode.io/problems/binary-tree-preorder-traversal/question?list=neetcode250
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public IList<int> PreorderTraversal(TreeNode root)
+    {
+        var result = new List<int>();
+
+        if (root is null)
+        {
+            return result;
+        }
+
+        Stack<TreeNode> stack = new();
+        TreeNode current = null;
+
+        stack.Push(root);
+        while (stack.Count > 0)
+        {
+            current = stack.Pop();
+            result.Add(current.val);
+
+            if (current.right is not null)
+            {
+                stack.Push(current.right);
+            }
+            if (current.left is not null)
+            {
+                stack.Push(current.left);
+            }
+        }
+
+        return result;
+    }
+}
