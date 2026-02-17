a78ad02df99d9fa249ecb865245fcc9fece61026
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-17 10:15:33 +0100

    feat: implement N-ary Tree Postorder Traversal solution

---
+++ neetcode/P0590_NaryTreePostorderTraversal/Solution.cs
@@ -0,0 +1,79 @@
+﻿namespace neetcode.P0590_NaryTreePostorderTraversal;
+
+public class Node
+{
+    public int val;
+    public IList<Node> children;
+
+    public Node() { }
+
+    public Node(int _val)
+    {
+        val = _val;
+    }
+
+    public Node(int _val, IList<Node> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+}
+
+// https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+// https://neetcode.io/problems/n-ary-tree-postorder-traversal/question?list=allNC
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, IList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+public class Solution
+{
+    public IList<int> Postorder(Node root)
+    {
+        List<int> result = new();
+
+        if (root is null)
+        {
+            return result;
+        }
+
+        Stack<Node> stack = new();
+
+        stack.Push(root);
+        while (stack.Count > 0)
+        {
+            Node current = stack.Pop();
+            result.Add(current.val);
+
+            if (current.children is null || current.children.Count == 0)
+            {
+                continue;
+            }
+
+            for (int i = 0; i < current.children.Count; i++)
+            {
+                if (current.children[i] is not null)
+                {
+                    stack.Push(current.children[i]);
+                }
+            }
+        }
+
+        result.Reverse();
+
+        return result;
+    }
+}
