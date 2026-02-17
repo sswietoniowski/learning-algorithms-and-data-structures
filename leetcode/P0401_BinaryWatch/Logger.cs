93fadd06341268c072992829487c19878a68737e
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-17 09:49:30 +0100

    feat: implement solution for Binary Watch problem

---
+++ leetcode/P0401_BinaryWatch/Logger.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode.P0401_BinaryWatch;
+
+using System.Numerics;
+
+// https://leetcode.com/problems/binary-watch/description/?envType=daily-question&envId=2026-02-17
+public class Solution
+{
+    public IList<string> ReadBinaryWatch(int turnedOn)
+    {
+        IList<string> result = new List<string>();
+
+        for (int h = 0; h < 12; h++)
+        {
+            for (int m = 0; m < 60; m++)
+            {
+                if (BitOperations.PopCount((uint)h) + BitOperations.PopCount((uint)m) == turnedOn)
+                {
+                    result.Add($"{h}:{m:D2}");
+                }
+            }
+        }
+
+        return result;
+    }
+}
