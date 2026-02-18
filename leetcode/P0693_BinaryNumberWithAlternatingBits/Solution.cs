4ea29f98d96e28577f288ad935fe7ec69e86ce97
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-18 12:02:51 +0100

    feat: add solution for Binary Number with Alternating Bits problem

---
+++ leetcode/P0693_BinaryNumberWithAlternatingBits/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode.P0693_BinaryNumberWithAlternatingBits;
+
+// https://leetcode.com/problems/binary-number-with-alternating-bits/description/?envType=daily-question&envId=2026-02-18
+public class Solution
+{
+    public bool HasAlternatingBits(int n)
+    {
+        uint result = (uint)n ^ ((uint)n >> 1);
+        return (result & (result + 1)) == 0;
+    }
+}
