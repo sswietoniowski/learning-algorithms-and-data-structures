17766e615c3c3cc38ba62f424ed474cc13ba9c77
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-19 06:33:39 +0100

    feat: add optimized solution for Rotate Array problem

---
+++ neetcode/P0189_RotateArray/Solution.cs
@@ -0,0 +1,51 @@
+﻿namespace neetcode.P0189_RotateArray;
+
+// https://leetcode.com/problems/rotate-array/description/
+// https://neetcode.io/problems/rotate-array/solution
+
+// v1
+/*
+public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+        int n = nums.Length;
+        k = k % n;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            result[(i + k) % n] = nums[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] = result[i];
+        }
+    }
+}
+*/
+// v2
+public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+        int n = nums.Length;
+        k %= n;
+        int count = 0;
+
+        for (int start = 0; count < n; start++)
+        {
+            int current = start;
+            int prev = nums[start];
+
+            do
+            {
+                int nextIdx = (current + k) % n;
+                int temp = nums[nextIdx];
+                nums[nextIdx] = prev;
+                prev = temp;
+                current = nextIdx;
+                count++;
+            } while (start != current);
+        }
+    }
+}
