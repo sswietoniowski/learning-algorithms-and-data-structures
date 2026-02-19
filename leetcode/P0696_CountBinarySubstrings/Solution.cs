9269890130896a506b7f30c29232cf6db5852b00
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-19 06:26:20 +0100

    feat: add solution for Count Binary Substrings problem

---
+++ leetcode/P0696_CountBinarySubstrings/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode.P0696_CountBinarySubstrings;
+
+// https://leetcode.com/problems/count-binary-substrings/description/?envType=daily-question&envId=2026-02-19
+public class Solution
+{
+    public int CountBinarySubstrings(string s)
+    {
+        int count = 0;
+        int prevGroupLength = 0;
+        int currGroupLength = 1;
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (s[i] == s[i - 1])
+            {
+                currGroupLength++;
+            }
+            else
+            {
+                count += Math.Min(prevGroupLength, currGroupLength);
+
+                prevGroupLength = currGroupLength;
+                currGroupLength = 1;
+            }
+        }
+
+        count += Math.Min(prevGroupLength, currGroupLength);
+
+        return count;
+    }
+}
