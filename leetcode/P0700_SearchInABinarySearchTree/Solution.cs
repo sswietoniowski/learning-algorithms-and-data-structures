08bb03b3c27cd57e62b6a452fe06b47f8ead32ac
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-20 17:26:29 +0100

    feat: add solution for Search in a Binary Search Tree problem

---
+++ leetcode/P0700_SearchInABinarySearchTree/Solution.cs
@@ -0,0 +1,65 @@
+﻿namespace leetcode.P0700_SearchInABinarySearchTree;
+
+// https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public TreeNode SearchBST(TreeNode root, int val)
+    {
+        if (root is null)
+        {
+            return null;
+        }
+
+        if (root.left is null && root.right is null)
+        {
+            if (root.val == val)
+            {
+                return root;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        if (root.val == val)
+        {
+            return root;
+        }
+        else if (root.val < val)
+        {
+            return SearchBST(root.right, val);
+        }
+        else
+        {
+            return SearchBST(root.left, val);
+        }
+    }
+}
