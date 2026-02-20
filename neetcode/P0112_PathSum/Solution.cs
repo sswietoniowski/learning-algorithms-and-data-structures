6d1184d8fa3dcba4da4c5830a07591bf659ee932
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-20 08:36:48 +0100

    feat: add solution for Path Sum problem

---
+++ neetcode/P0112_PathSum/Solution.cs
@@ -0,0 +1,49 @@
+﻿namespace neetcode.P0112_PathSum;
+
+// https://leetcode.com/problems/path-sum/description/
+// https://neetcode.io/problems/path-sum/question
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public bool HasPathSum(TreeNode root, int targetSum)
+    {
+        if (root is null)
+        {
+            return false;
+        }
+
+        if (root.left is null && root.right is null && root.val == targetSum)
+        {
+            return true;
+        }
+
+        return HasPathSum(root.left, targetSum - root.val)
+            || HasPathSum(root.right, targetSum - root.val);
+    }
+}
