0b923d682c7c56a58ac014fed4905b10bd96f4a1
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-20 17:32:23 +0100

    feat: add solution for Root Equals Sum of Children problem

---
+++ leetcode/P2236_RootEqualsSumOfChildren/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace leetcode.P2236_RootEqualsSumOfChildren;
+
+// https://leetcode.com/problems/root-equals-sum-of-children/description/
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public bool CheckTree(TreeNode root)
+    {
+        return root.val == (root.left.val + root.right.val);
+    }
+}
