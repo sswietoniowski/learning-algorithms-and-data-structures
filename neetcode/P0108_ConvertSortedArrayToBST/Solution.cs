e6d22e1ba38b2b42f9f639c4a3ac9a578183699a
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-21 08:19:35 +0100

    feat: add solution for Convert Sorted Array to Binary Search Tree problem

---
+++ neetcode/P0108_ConvertSortedArrayToBST/Solution.cs
@@ -0,0 +1,54 @@
+﻿namespace neetcode.P0108_ConvertSortedArrayToBST;
+
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution
+{
+    public TreeNode SortedArrayToBST(int[] nums)
+    {
+        return BuildTree(nums, 0, nums.Length - 1);
+    }
+
+    private TreeNode BuildTree(int[] nums, int left, int right)
+    {
+        if (left > right)
+        {
+            return null;
+        }
+
+        int middle = left + (right - left) / 2;
+
+        var node = new TreeNode(nums[middle]);
+        node.left = BuildTree(nums, left, middle - 1);
+        node.right = BuildTree(nums, middle + 1, right);
+
+        return node;
+    }
+}
