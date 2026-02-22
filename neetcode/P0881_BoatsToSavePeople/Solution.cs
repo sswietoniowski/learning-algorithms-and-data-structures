d755a273553806d3ddf5d5108e6f1420c9d5dd7a
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-22 07:02:26 +0100

    feat: implement NumRescueBoats method to calculate minimum number of boats required to save people

---
+++ neetcode/P0881_BoatsToSavePeople/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace neetcode.P0881_BoatsToSavePeople;
+
+// https://leetcode.com/problems/boats-to-save-people/
+// https://neetcode.io/problems/boats-to-save-people/history?submissionIndex=0
+public class Solution
+{
+    public int NumRescueBoats(int[] people, int limit)
+    {
+        Array.Sort(people);
+
+        int boatCount = 0;
+        int left = 0;
+        int right = people.Length - 1;
+
+        while (left <= right)
+        {
+            if (people[left] + people[right] <= limit)
+            {
+                left++;
+            }
+
+            right--;
+
+            boatCount++;
+        }
+
+        return boatCount;
+    }
+}
