95229797817702f30e88952fb5bbdd0491f91527
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-22 06:57:25 +0100

    feat: implement BinaryGap method to calculate maximum distance between binary 1s

---
+++ leetcode/P0868_BinaryGap/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode.P0868_BinaryGap;
+
+// https://leetcode.com/problems/binary-gap/description/?envType=daily-question&envId=2026-02-22
+public class Solution
+{
+    public int BinaryGap(int n)
+    {
+        int maxDistance = 0;
+        int lastPos = -1;
+        int currentPos = 0;
+
+        while (n > 0)
+        {
+            if ((n & 1) == 1)
+            {
+                if (lastPos != -1)
+                {
+                    maxDistance = Math.Max(maxDistance, currentPos - lastPos);
+                }
+                lastPos = currentPos;
+            }
+
+            n >>= 1;
+            currentPos++;
+        }
+
+        return maxDistance;
+    }
+}
