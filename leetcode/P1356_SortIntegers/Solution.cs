8f6cfab85f9747f00ec59e28465292629618a7a7
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-25 07:42:52 +0100

    feat: implement SortByBits method to sort integers by the number of 1 bits

---
+++ leetcode/P1356_SortIntegers/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode.P1356_SortIntegers;
+
+// https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/description/?envType=daily-question&envId=2026-02-25
+using System.Numerics;
+
+public class Solution
+{
+    public int[] SortByBits(int[] arr)
+    {
+        Array.Sort(
+            arr,
+            (a, b) =>
+            {
+                int aBits = BitOperations.PopCount((uint)a);
+                int bBits = BitOperations.PopCount((uint)b);
+                int result = aBits.CompareTo(bBits);
+                return result != 0 ? result : a.CompareTo(b);
+            }
+        );
+        return arr;
+    }
+}
