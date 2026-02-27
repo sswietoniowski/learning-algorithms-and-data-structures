2d9a05317bbdd2098e9c859b2a9ebdb063f8c843
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-27 15:25:22 +0100

    feat: implement Combine method to generate all combinations of k numbers from 1 to n

---
+++ neetcode/P0077_Combinations/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace neetcode.P0077_Combinations;
+
+// https://leetcode.com/problems/combinations/description/
+// https://neetcode.io/problems/combinations/question
+public class Solution
+{
+    public IList<IList<int>> Combine(int n, int k)
+    {
+        var combinations = new List<IList<int>>();
+
+        void Backtrack(int i, List<int> combination)
+        {
+            if (i > n)
+            {
+                if (combination.Count == k)
+                {
+                    combinations.Add([.. combination]);
+                }
+                return;
+            }
+
+            combination.Add(i);
+            Backtrack(i + 1, combination);
+            combination.RemoveAt(combination.Count - 1);
+            Backtrack(i + 1, combination);
+        }
+
+        Backtrack(1, new List<int>());
+
+        return combinations;
+    }
+}
