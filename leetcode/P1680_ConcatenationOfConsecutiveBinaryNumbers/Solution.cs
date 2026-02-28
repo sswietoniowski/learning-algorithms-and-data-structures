877955ac9daf651c80046d3aca479bb89a70057f
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-02-28 07:12:30 +0100

    feat: implement ConcatenatedBinary method to calculate the concatenation of consecutive binary numbers

---
+++ leetcode/P1680_ConcatenationOfConsecutiveBinaryNumbers/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode.P1680_ConcatenationOfConsecutiveBinaryNumbers;
+
+// https://leetcode.com/problems/concatenation-of-consecutive-binary-numbers/description/?envType=daily-question&envId=2026-02-28
+public class Solution
+{
+    public int ConcatenatedBinary(int n)
+    {
+        long result = 0;
+        int MOD = 1_000_000_007;
+        int binaryLength = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if ((i & (i - 1)) == 0)
+            {
+                binaryLength++;
+            }
+
+            result = ((result << binaryLength) | (uint)i) % MOD;
+        }
+
+        return (int)result;
+    }
+}
