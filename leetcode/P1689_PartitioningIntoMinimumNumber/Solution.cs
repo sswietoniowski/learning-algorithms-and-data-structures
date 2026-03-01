6c0d54da2f82940a55fc1e1e31c6a4eef727091d
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-03-01 07:23:04 +0100

    feat: implement MinPartitions method to determine the minimum number of deci-binary numbers needed to sum to a given number

---
+++ leetcode/P1689_PartitioningIntoMinimumNumber/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode.P1689_PartitioningIntoMinimumNumber;
+
+// https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/?envType=daily-question&envId=2026-03-01
+public class Solution
+{
+    public int MinPartitions(string n)
+    {
+        int maxDigit = 0;
+
+        foreach (char c in n)
+        {
+            int currentDigit = c - '0';
+
+            if (currentDigit > maxDigit)
+            {
+                maxDigit = currentDigit;
+            }
+
+            if (maxDigit == 9)
+                return 9;
+        }
+
+        return maxDigit;
+    }
+}
