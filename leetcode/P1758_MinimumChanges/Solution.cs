eae92d8bf7d70207d0ccad6deba7161d95428d32
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-03-05 06:25:23 +0100

    feat: implement MinOperations method to calculate minimum changes for alternating binary string

---
+++ leetcode/P1758_MinimumChanges/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode.P1758_MinimumChanges;
+
+// https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string/description/?envType=daily-question&envId=2026-03-05
+public class Solution
+{
+    public int MinOperations(string s)
+    {
+        int count0 = 0;
+        int n = s.Length;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0)
+            {
+                if (s[i] != '0')
+                    count0++;
+            }
+            else
+            {
+                if (s[i] != '1')
+                    count0++;
+            }
+        }
+
+        return Math.Min(count0, n - count0);
+    }
+}
