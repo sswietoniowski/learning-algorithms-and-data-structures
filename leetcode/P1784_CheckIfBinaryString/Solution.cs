de139f1cd734fa64ff9a14a1a56967a6f714eb96
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-03-06 07:48:44 +0100

    feat: implement CheckOnesSegment method to verify if a binary string has at most one segment of ones

---
+++ leetcode/P1784_CheckIfBinaryString/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace leetcode.P1784_CheckIfBinaryString;
+
+// https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/description/?envType=daily-question&envId=2026-03-06
+public class Solution
+{
+    public bool CheckOnesSegment(string s)
+    {
+        return !s.Contains("01");
+    }
+}
