aeb21ebaad128c057d31b1530e526bad6c9ebd36
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-03-08 07:35:22 +0100

    feat: implement FindDifferentBinaryString method to generate a unique binary string from input array

---
+++ leetcode/P1980_FindUniqueBinaryString/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace leetcode.P1980_FindUniqueBinaryString;
+
+// https://leetcode.com/problems/find-unique-binary-string/description/?envType=daily-question&envId=2026-03-08
+public class Solution
+{
+    public string FindDifferentBinaryString(string[] nums)
+    {
+        StringBuilder result = new StringBuilder();
+        int n = nums.Length;
+
+        for (int i = 0; i < n; i++)
+        {
+            // Get the i-th character of the i-th string
+            char currentBit = nums[i][i];
+
+            // Flip it: if it's '0', add '1'; if it's '1', add '0'
+            result.Append(currentBit == '0' ? '1' : '0');
+        }
+
+        return result.ToString();
+    }
+}
