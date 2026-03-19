50cb9faf733c46576822a50429acba3b9c6030d6
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-03-19 07:27:26 +0100

    feat: implement RangeBitwiseAnd method to calculate bitwise AND of a range of numbers

---
+++ neetcode/P0201_BitwiseANDOfNumbersRange/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace neetcode.P0201_BitwiseANDOfNumbersRange;
+
+// https://leetcode.com/problems/bitwise-and-of-numbers-range/description/
+// https://neetcode.io/problems/bitwise-and-of-numbers-range/question?list=neetcode250
+public class Solution
+{
+    public int RangeBitwiseAnd(int left, int right)
+    {
+        int shift = 0;
+
+        // While the numbers are different, they haven't reached the common prefix
+        while (left < right)
+        {
+            left >>= 1;
+            right >>= 1;
+            shift++;
+        }
+
+        // Shift the common prefix back to its original position
+        return left << shift;
+    }
+}
