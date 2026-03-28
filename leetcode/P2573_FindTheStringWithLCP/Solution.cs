893e47dd664f05c6f7a5f81139b65fdf2978458b
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-03-28 10:03:15 +0100

    feat: implement FindTheString method to construct a string based on the given LCP matrix

---
+++ leetcode/P2573_FindTheStringWithLCP/Solution.cs
@@ -0,0 +1,52 @@
+﻿namespace leetcode.P2573_FindTheStringWithLCP;
+
+// https://leetcode.com/problems/find-the-string-with-lcp/description/?envType=daily-question&envId=2026-03-28
+public class Solution
+{
+    public string FindTheString(int[][] lcp)
+    {
+        int n = lcp.Length;
+        char[] s = new char[n];
+        char c = 'a';
+
+        // 1. Greedy Construction
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] != '\0')
+                continue; // Already assigned
+            if (c > 'z')
+                return ""; // Out of lowercase letters
+
+            for (int j = i; j < n; j++)
+            {
+                if (lcp[i][j] > 0)
+                {
+                    s[j] = c;
+                }
+            }
+            c++;
+        }
+
+        // 2. Validation
+        // We must check if the string we built actually produces the input matrix
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = n - 1; j >= 0; j--)
+            {
+                int expectedLcp = 0;
+                if (s[i] == s[j])
+                {
+                    // Standard LCP DP: 1 + LCP of the next suffixes
+                    expectedLcp = (i + 1 < n && j + 1 < n) ? lcp[i + 1][j + 1] + 1 : 1;
+                }
+
+                if (lcp[i][j] != expectedLcp)
+                {
+                    return "";
+                }
+            }
+        }
+
+        return new string(s);
+    }
+}
