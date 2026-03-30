72562fd0c5c9fcdcf030cd346044c09670efbf2f
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2026-03-30 09:57:05 +0200

    feat: implement CheckStrings method to determine if two strings can be made equal with specific operations

---
+++ leetcode/P2840_CheckIfStringsCanBeMadeEqualWithOperationsII/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace leetcode.P2840_CheckIfStringsCanBeMadeEqualWithOperationsII;
+
+// https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-ii/description/?envType=daily-question&envId=2026-03-30
+public class Solution
+{
+    public bool CheckStrings(string s1, string s2)
+    {
+        int n = s1.Length;
+
+        // Frequency arrays for 'a' through 'z' (26 letters)
+        int[] evenCounts = new int[26];
+        int[] oddCounts = new int[26];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0)
+            {
+                // For even indices: increment for s1, decrement for s2
+                evenCounts[s1[i] - 'a']++;
+                evenCounts[s2[i] - 'a']--;
+            }
+            else
+            {
+                // For odd indices: increment for s1, decrement for s2
+                oddCounts[s1[i] - 'a']++;
+                oddCounts[s2[i] - 'a']--;
+            }
+        }
+
+        // If the strings can be made equal, all frequency counts must be zero
+        for (int i = 0; i < 26; i++)
+        {
+            if (evenCounts[i] != 0 || oddCounts[i] != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
